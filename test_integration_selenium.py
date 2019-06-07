05c78ac707d286d3cb37a08fe82a75f51ad4a756
Dominic May <me@mause.me>
2019-06-07 19:21:07 +0800

    Complete simple selenium test

--- test_integration_selenium.py
+++ test_integration_selenium.py
@@ -1,6 +1,9 @@
-import time
-from pytest import fixture
+from urllib.parse import urlencode, urlparse
+
 import selenium.webdriver
+from pytest import fixture
+
+url = 'http://cornell.local:5000'
 
 
 @fixture(scope='module')
@@ -14,32 +17,40 @@ def webdriver():
         driver.quit()
 
 
-url = 'http://cornell.local:5000'
-
-
 def click_link(webdriver, text):
-    link = webdriver.find_elements_by_partial_link_text(text)
-    assert link, f'Cannot find link with text "{text}"'
-    link[0].click()
+    webdriver.find_element_by_link_text(text).click()
+
+
+def search(webdriver, text):
+    form = webdriver.find_element_by_tag_name('form')
+    form.find_element_by_name('query').send_keys(text)
+    form.submit()
 
 
 def test_simple(webdriver):
     webdriver.get(url)
 
-    # __import__('pdb').set_trace()
-
-    form = webdriver.find_element_by_tag_name('form')
-
-    input_ = form.find_element_by_name('query')
-
-    input_.send_keys('chernobyl')
-
-    form.submit()
-
-    webdriver.find_element_by_xpath
+    search(webdriver, 'chernobyl')
 
     click_link(webdriver, 'Chernobyl (2019)')
     click_link(webdriver, 'Season 1')
     click_link(webdriver, '1:23:45')
 
-    click_link(webdriver, 'Chernobyl 1080p')
+    anchor = urlparse(
+        webdriver.find_element_by_partial_link_text(
+            'Chernobyl.S01E01.iNTERNAL.1080p.WEB.H264-EDHD[rartv]'
+        ).get_attribute('href')
+    )
+    expected = urlparse(
+        'http://cornell.local:5000/download/series?'
+        + urlencode(
+            {
+                'magnet': 'magnet:?xt=urn:btih:0a49edcbe6cfca62a7b8b24a7f60094b697aa2e9&dn=Chernobyl.S01E01.iNTERNAL.1080p.WEB.H264-EDHD%5Brartv%5D&tr=http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2710&tr=udp%3A%2F%2F9.rarbg.to%3A2710&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce',
+                'imdb_id': 'tt7366338',
+                'titles': '1:23:45',  # - Chernobyl',
+                'season': '1',
+                'episode': '1',
+            }
+        )
+    )
+    assert anchor == expected
