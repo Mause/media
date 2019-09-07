ca6e8c7cb67d7a819837b209dffa6a5b10120bde
Dominic May <me@mause.me>
2019-09-07 21:35:56 +0800

    Point to postgres instance

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,9 @@
+import os
 from main import create_app
 
 app = create_app(
-    {'TRANSMISSION_URL': 'http://novell.mause.me:9091/transmission/rpc'}
+    {
+        'TRANSMISSION_URL': 'http://novell.mause.me:9091/transmission/rpc',
+        'SQLALCHEMY_DATABASE_URI': os.environ['DATABASE_URL'],
+    }
 )
