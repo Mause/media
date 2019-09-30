d06712bd112b84ff6f03947671cde661bf855634
Dominic May <me@mause.me>
2019-09-30 20:21:24 +0800

    Add discover function to tmdb

--- tmdb.py
+++ tmdb.py
@@ -1,11 +1,11 @@
 from datetime import date
+from enum import Enum
 from itertools import chain
 from typing import Dict, List, Optional
 
 import backoff
 import requests
 from requests_toolbelt.sessions import BaseUrlSession
-
 from utils import lru_cache
 
 tmdb = BaseUrlSession('https://api.themoviedb.org/3/')
@@ -57,9 +57,7 @@ def search_themoviedb(s: str) -> List[Dict]:
 @lru_cache()
 def find_themoviedb(i: str):
     assert i.startswith('tt')
-    results = tmdb.get(
-        f'find/{i}', params={'external_source': 'imdb_id'}
-    ).json()
+    results = tmdb.get(f'find/{i}', params={'external_source': 'imdb_id'}).json()
 
     result = next(item for item in chain.from_iterable(results.values()))
 
@@ -69,9 +67,7 @@ def find_themoviedb(i: str):
 @lru_cache()
 def resolve_id(imdb_id: str) -> str:
     assert imdb_id.startswith('tt')
-    results = tmdb.get(
-        f'find/{imdb_id}', params={'external_source': 'imdb_id'}
-    ).json()
+    results = tmdb.get(f'find/{imdb_id}', params={'external_source': 'imdb_id'}).json()
 
     res = next((item for item in chain.from_iterable(results.values())), None)
     assert res, f'No results for {imdb_id}'
@@ -104,3 +100,23 @@ def get_imdb_id(type: str, id: str) -> str:
 @lru_cache()
 def get_tv_episodes(id: str, season: str):
     return get_json(f'tv/{id}/season/{season}')
+
+
+class ReleaseType(Enum):
+    PREMIERE = 1
+    THEATRICAL_LIMITED = 2
+    THEATRICAL = 3
+    DIGITAL = 4
+    PHYSICAL = 5
+    TV = 6
+
+
+def discover(types=(ReleaseType.PHYSICAL, ReleaseType.DIGITAL)):
+    return get_json(
+        'discover/movie',
+        params={
+            'sort_by': ','.join(('popularity.desc', 'primary_release_date.desc')),
+            'primary_release_date.lte': date.today().isoformat(),
+            'with_release_type': '|'.join(str(ReleaseType(i).value) for i in types),
+        },
+    )
