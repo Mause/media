89228713541d2f7044fd99ae71cc1c9127df8a3a
Dominic May <me@mause.me>
2020-01-02 21:06:58 +0800

    Add sentry

--- rarbg_local/wsgi.py
+++ rarbg_local/wsgi.py
@@ -1,7 +1,10 @@
 import os
 
+import sentry_sdk
+
 from .main import create_app
 
+sentry_sdk.init(os.environ['SENTRY_DSN'])
 app = create_app(
     {
         'TRANSMISSION_URL': 'http://novell.mause.me:9091/transmission/rpc',
