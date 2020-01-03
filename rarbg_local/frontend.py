837213eeb643468a521b8ac23b967ef0ae97b09c
Dominic May <me@mause.me>
2020-01-03 18:20:08 +0800

    Use browserify instead of handrolling things

--- rarbg_local/frontend.py
+++ rarbg_local/frontend.py
@@ -1,91 +1,27 @@
 from os.path import abspath
 
 from flask_assets import Bundle, Environment
-from webassets.filter import ExternalTool, Filter
-from webassets.filter.requirejs import RequireJSFilter
+from webassets.filter import ExternalTool
 
 e = Environment()
 
 
-class TypeScript(ExternalTool):
-    def input(self, _in, out, **kw):
+class Browserify(ExternalTool):
+    def input(self, data, out, **kwargs):
         argv = [
-            abspath('node_modules/.bin/tsc'),
-            '--outFile',
-            '{output}',
-            '--jsx',
-            'react',
-            '--module',
-            'amd',
-            '--esModuleInterop',
-            'true',
-            "--moduleResolution",
-            "node",
-            '--lib',
-            'es2017,dom',
-            kw['source_path'],
+            abspath('node_modules/.bin/browserify'),
+            '-p',
+            '[',
+            'tsify',
+            ']',
+            kwargs['source_path'],
         ]
-        cwd = abspath('.')
-        return self.subprocess(argv, out, cwd=cwd)
+        return self.subprocess(argv, out, data, cwd=abspath('.'))
 
-
-dep = "https://cdnjs.cloudflare.com/ajax/libs/{}/{}/{}".format
-urls = [
-    dep("lodash.js", "4.17.15", "lodash.js"),
-    dep("react-dom", "16.11.0", "umd/react-dom.development.js"),
-    dep("react", "16.12.0", "umd/react.development.js"),
-    dep("qs", "6.9.1", "qs.min.js"),
-    dep("react-router", "5.1.2", "react-router.js"),
-    dep("react-router-dom", "5.1.2", "react-router-dom.js"),
-    dep('axios', "0.19.0", "axios.js"),
-]
-
-
-def get_name(url: str) -> str:
-    return url.split('/')[-1].split('.')[0]
-
-
-class Namer(Filter):
-    def input(self, in_, out, **kwargs):
-        template = """
-        (function(){
-            let captureDeps, captureCb;
-
-            (function(){
-                function define(valueDeps, valueCb) {
-                    captureDeps = valueDeps;
-                    captureCb = valueCb;
-                }
-                define.amd = {};
-                \n %s \n
-            })()
-
-            define('%s', captureDeps, captureCb);
-        })();
-        """
-
-        name = get_name(kwargs['source_path'])
-        out.write(template % (in_.read(), name))
-
-
-e.register(
-    'deps',
-    Bundle(
-        *urls,
-        extra={'names': list(map(get_name, urls))},
-        filters=[Namer()],
-        output='gen/deps.js'
-    ),
-)
 
 e.register(
     'streaming',
-    Bundle(
-        'streaming.tsx',
-        filters=[TypeScript()],
-        extra={'names': ['streaming']},
-        output='gen/streaming.js',
-    ),
+    Bundle('streaming.tsx', filters=[Browserify()], output='gen/streaming.js'),
 )
 
 
@@ -95,4 +31,6 @@ if __name__ == '__main__':
     f = Flask(__name__)
 
     with f.app_context():
-        e['deps'].build(force=True)
+        e.cache = False
+        e.manifest = False
+        e['streaming'].build(force=True)
