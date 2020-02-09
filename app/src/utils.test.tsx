5068def3f875e700431867e7bcad2d94d40ff67f
Dominic May <me@mause.me>
2020-02-09 17:47:54 +0800

    Add missing file

---
+++ app/src/utils.test.tsx
@@ -0,0 +1,20 @@
+import moxios from "moxios";
+
+export function wait() {
+  return new Promise(resolve => moxios.wait(resolve));
+}
+
+export function mock<T>(path: string, response: T) {
+  moxios.stubOnce('GET', new RegExp(path), {
+    response,
+  });
+}
+
+export function useMoxios() {
+  beforeEach(() => {
+    moxios.install();
+  });
+  afterEach(() => {
+    moxios.uninstall();
+  });
+}
