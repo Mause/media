a79c98c32c4b369c3ca80829953b8ef1d0a54312
Dominic May <me@mause.me>
2020-02-14 19:36:59 +0800

    Add tv render test

--- app/src/render.test.tsx
+++ app/src/render.test.tsx
@@ -1,27 +1,44 @@
-import { act, render } from "@testing-library/react";
-import React from "react";
-import { Movies, TvShows } from "./render";
-import { Route, MemoryRouter } from "react-router-dom";
-import { mock, wait, useMoxios } from "./test.utils";
+import { act, render } from '@testing-library/react';
+import React from 'react';
+import { Movies, TVShows } from './render';
+import { Route, MemoryRouter } from 'react-router-dom';
+import { mock, wait, useMoxios } from './test.utils';
 
 useMoxios();
 
-test("Movies", async () => {
-  await act(async () => {
-    const movies: SearchResult[] = [
-      {
-        id: 1,
-        download: {
-            title: "Hello"
-            ,
-            added_by: {
-                first_name: 'David'
-            }
-        }
-      }
-    ];
+test('Movies', () => {
+  const movies: Movie[] = [
+    {
+      id: 1,
+      download: {
+        title: 'Hello',
+        added_by: {
+          first_name: 'David',
+        },
+      },
+    },
+  ];
 
-    const el = render(<Movies movies={movies} />);
+  const el = render(<Movies movies={movies} />);
+
+  expect(el).toMatchSnapshot();
+});
+
+test('TVShows', async () => {
+  await act(async () => {
+    const series: TV[] = [
+      {
+        tmdb_id: 1,
+        seasons: {
+          1: [],
+        },
+      },
+    ];
+    const el = render(
+      <MemoryRouter>
+        <TVShows series={series} />
+      </MemoryRouter>,
+    );
 
     expect(el).toMatchSnapshot();
   });
