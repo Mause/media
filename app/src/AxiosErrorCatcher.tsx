2dc47586fc9448554026c62ce9cf72e722dff475
Dominic May <me@mause.me>
2020-02-16 13:23:55 +0800

    Add error catcher for axios

---
+++ app/src/AxiosErrorCatcher.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function AxiosErrorCatcher(props: { children: any }) {
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const reqId = axios.interceptors.request.use(req => {
+      setError(null);
+      return req;
+    });
+    const resId = axios.interceptors.response.use(req => req, setError);
+
+    return () => {
+      axios.interceptors.request.eject(reqId);
+      axios.interceptors.response.eject(resId);
+    };
+  }, []);
+
+  if (error) throw error;
+
+  return props.children;
+}
