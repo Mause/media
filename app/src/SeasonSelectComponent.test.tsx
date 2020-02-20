17fef4a332c515f33047adfb3c7df3456f46f929
Dominic May <me@mause.me>
2020-02-20 10:02:51 +0800

    Format

--- app/src/SeasonSelectComponent.test.tsx
+++ app/src/SeasonSelectComponent.test.tsx
@@ -1,8 +1,13 @@
-import { act, render } from "@testing-library/react";
-import React from "react";
-import { MemoryRouter, Route } from "react-router-dom";
-import { EpisodeSelectComponent, Season, SeasonSelectComponent, TV } from "./SeasonSelectComponent";
-import { mock, useMoxios, wait } from "./test.utils";
+import { act, render } from '@testing-library/react';
+import React from 'react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import {
+  EpisodeSelectComponent,
+  Season,
+  SeasonSelectComponent,
+  TV,
+} from './SeasonSelectComponent';
+import { mock, useMoxios, wait } from './test.utils';
 import { swrConfig } from './streaming';
 
 useMoxios();
@@ -22,11 +27,11 @@ test('SeasonSelectComponent  render', async () => {
     await mock<TV>('/api/tv/1', {
       title: 'Hello',
       number_of_seasons: 1,
-      seasons: [{ episode_count: 1 }]
+      seasons: [{ episode_count: 1 }],
     });
     await wait();
 
-    expect(el.getByTestId('title').textContent).toEqual("Hello");
+    expect(el.getByTestId('title').textContent).toEqual('Hello');
     expect(el).toMatchSnapshot();
   });
 });
@@ -44,15 +49,17 @@ test('EpisodeSelectComponent render', async () => {
     );
 
     await mock<Season>('/api/tv/1/season/1', {
-      episodes: [{
-        episode_number: 1,
-        id: '1',
-        name: 'Episode 1',
-      }],
+      episodes: [
+        {
+          episode_number: 1,
+          id: '1',
+          name: 'Episode 1',
+        },
+      ],
     });
     await wait();
 
-    expect(el.getByTestId('title').textContent).toEqual("Season 1");
+    expect(el.getByTestId('title').textContent).toEqual('Season 1');
     expect(el).toMatchSnapshot();
   });
 });
