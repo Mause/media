71759d7da760e58a11b4949ed9154def138d1153
Dominic May <me@mause.me>
2020-02-21 19:05:33 +0800

    Add MLink

--- app/src/utils.tsx
+++ app/src/utils.tsx
@@ -1,12 +1,19 @@
 import Axios from 'axios';
 import { useState, useEffect } from 'react';
 import qs from 'qs';
+import MaterialLink from '@material-ui/core/Link';
+import { Link } from 'react-router-dom';
+import { LocationDescriptor } from 'history';
 
 import axiosRetry from '@vtex/axios-concurrent-retry';
 
 axiosRetry(Axios, { retries: 3 });
 
 export const BASE = window.location.host.includes('localhost') ? 'http://localhost:5000' : '';
+
+export function MLink<S>(props: { children: React.ReactNode, to: LocationDescriptor<S> }) {
+  return <MaterialLink color="inherit" component={Link} {...props} />;
+}
 
 export function subscribe(path: string, callback: (a: any) => void, end: (() => void) | null = null): void {
   const es = new EventSource(BASE + path, {
