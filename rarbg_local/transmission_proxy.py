868626cef2d69755d7af508bfc7370492a1eefce
Dominic May <me@mause.me>
2020-03-01 16:42:43 +0800

    Explicit imports and exports

--- rarbg_local/transmission_proxy.py
+++ rarbg_local/transmission_proxy.py
@@ -4,9 +4,12 @@ from typing import TYPE_CHECKING
 from .client import get_client
 
 if TYPE_CHECKING:
-    from .transmission import *
+    from .transmission import torrent_add, get_torrent
 else:
     transmission = lru_cache()(get_client)
 
     def __getattr__(name):
         return lambda *args, **kwargs: getattr(transmission(), name)(*args, **kwargs)
+
+
+__all__ = ['torrent_add', 'get_torrent']
