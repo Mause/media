b3ef75558876f7bf40f0f45fbfc6db35cab50f8b
Dominic May <me@mause.me>
2020-03-01 16:52:04 +0800

    Format SearchComponent

--- app/src/SearchComponent.tsx
+++ app/src/SearchComponent.tsx
@@ -16,17 +16,30 @@ export function SearchComponent() {
   const { search } = useLocation();
   const { query } = qs.parse(search.slice(1));
 
-  const results = useLoad<SearchResult[]>('search', { query, });
-  return <div>
-    <SearchBox />
-    <ul>
-      {results ? results.map(result => <li key={result.imdbID}>
-        <Link to={result.Type === 'movie' ?
-          `/select/${result.imdbID}/options` :
-          `/select/${result.imdbID}/season`}>{result.title} ({result.Year ? result.Year : 'Unknown year'})</Link>
-      </li>) : <ReactLoading type='balls' color='#000' />}
-    </ul>
-    {results && results.length === 0 ?
-      'No results' : null}
-  </div>;
+  const results = useLoad<SearchResult[]>('search', { query });
+  return (
+    <div>
+      <SearchBox />
+      <ul>
+        {results ? (
+          results.map(result => (
+            <li key={result.imdbID}>
+              <Link
+                to={
+                  result.Type === 'movie'
+                    ? `/select/${result.imdbID}/options`
+                    : `/select/${result.imdbID}/season`
+                }
+              >
+                {result.title} ({result.Year ? result.Year : 'Unknown year'})
+              </Link>
+            </li>
+          ))
+        ) : (
+          <ReactLoading type="balls" color="#000" />
+        )}
+      </ul>
+      {results && results.length === 0 ? 'No results' : null}
+    </div>
+  );
 }
