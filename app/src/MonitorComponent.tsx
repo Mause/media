6213e326c01241f69476aabe84215c2f4b9e8ced
Dominic May <me@mause.me>
2020-03-04 22:09:56 +0800

    Add search link to monitor list

--- app/src/MonitorComponent.tsx
+++ app/src/MonitorComponent.tsx
@@ -1,20 +1,38 @@
 import useSWR from 'swr';
 import React from 'react';
 import ReactLoading from 'react-loading';
-import { Redirect, useParams } from 'react-router-dom';
+import { Redirect, useParams, useHistory } from 'react-router-dom';
 import { usePost } from './utils';
+import { ContextMenu, MenuItem } from 'react-contextmenu';
+import { contextMenuTrigger } from './render';
 
 export function MonitorComponent() {
-  const { data } = useSWR<{ title: string }[]>('monitor');
+  const { data } = useSWR<{ title: string; id: number; tmdb_id: string }[]>(
+    'monitor',
+  );
+  const history = useHistory();
 
   return (
     <div>
       <h3>Monitored movies</h3>
       {data ? (
         <ul>
-          {data.map(m => (
-            <li>{m.title}</li>
-          ))}
+          {data.map(m => {
+            let id = `monitor_${m.id}`;
+            return (
+              <li>
+                {m.title}&nbsp;
+                {contextMenuTrigger(id)}
+                <ContextMenu id={id}>
+                  <MenuItem
+                    onClick={() => history.push(`/select/${m.tmdb_id}/options`)}
+                  >
+                    Search
+                  </MenuItem>
+                </ContextMenu>
+              </li>
+            );
+          })}
         </ul>
       ) : (
         <ReactLoading color="#000000" />
