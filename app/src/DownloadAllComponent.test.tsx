8de903bb03b6ef5aeee8d389692210db06ca5da7
Dominic May <me@mause.me>
2020-03-08 12:46:54 +0800

    Add DownloadAllComponent.test.tsx

---
+++ app/src/DownloadAllComponent.test.tsx
@@ -0,0 +1,41 @@
+import { renderWithSWR, mock, useMoxios, wait } from './test.utils';
+import { DownloadAllComponent } from './DownloadAllComponent';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { act } from 'react-dom/test-utils';
+import React from 'react';
+import { ITorrent } from './OptionsComponent';
+
+useMoxios();
+
+test('DownloadAllComponent', async () => {
+  await act(async () => {
+    const el = renderWithSWR(
+      <MemoryRouter initialEntries={['/select/1/season/1/download_all']}>
+        <Route path="/select/:tmdb_id/season/:season/download_all">
+          <DownloadAllComponent />
+        </Route>
+      </MemoryRouter>,
+    );
+
+    expect(el.container).toMatchSnapshot();
+
+    const packs: ITorrent[] = [
+      {
+        category: 'Movie',
+        download: 'magnet:....',
+        episode_info: { seasonnum: '1', epnum: '1' },
+        seeders: 5,
+        title: 'Hello World',
+      },
+    ];
+
+    await mock('/api/select/1/season/1/download_all', {
+      packs,
+      incomplete: [],
+      complete: [],
+    });
+    await wait();
+
+    expect(el.container).toMatchSnapshot();
+  });
+});
