cae6003e23a24454a1c202751f1d7ce7bae2c0ff
Dominic May <me@mause.me>
2020-03-23 08:28:15 +0800

    Remove unused mock

--- rarbg_local/tests/test_integration.py
+++ rarbg_local/tests/test_integration.py
@@ -170,11 +170,6 @@ def test_download(test_client, responses, add_torrent):
 def test_download_season_pack(test_client, responses, add_torrent):
     themoviedb(responses, '/tv/95792', {'name': 'Pocket Monsters'})
     themoviedb(responses, '/tv/95792/external_ids', {'imdb_id': 'ttwhatever'})
-    themoviedb(
-        responses,
-        '/tv/95792/season/1',
-        {'episodes': [None, {'name': "Satoshi, Go, and Lugia Go!"}]},
-    )
 
     magnet = 'magnet:?xt=urn:btih:dacf233f2586b49709fd3526b390033849438313&dn=%5BSome-Stuffs%5D_Pocket_Monsters_%282019%29_002_%281080p%29_%5BCCBE335E%5D.mkv&tr=http%3A%2F%2Fnyaa.tracker.wf%3A7777%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce'
 
