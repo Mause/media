0eadc6e96e91668731b01e968e8c8391c34831ae
Dominic May <me@mause.me>
2020-03-24 19:43:44 +0800

    Modify test data

--- rarbg_local/tests/test_integration.py
+++ rarbg_local/tests/test_integration.py
@@ -157,17 +157,17 @@ def test_download(test_client, responses, add_torrent):
 
 
 def test_download_season_pack(test_client, responses, add_torrent):
-    themoviedb(responses, '/tv/95792', {'name': 'Pocket Monsters'})
-    themoviedb(responses, '/tv/95792/external_ids', {'imdb_id': 'ttwhatever'})
+    themoviedb(responses, '/tv/90000', {'name': 'Watchmen'})
+    themoviedb(responses, '/tv/90000/external_ids', {'imdb_id': 'ttwhatever'})
 
     magnet = 'magnet:?xt=urn:btih:dacf233f2586b49709fd3526b390033849438313&dn=%5BSome-Stuffs%5D_Pocket_Monsters_%282019%29_002_%281080p%29_%5BCCBE335E%5D.mkv&tr=http%3A%2F%2Fnyaa.tracker.wf%3A7777%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce'
 
     res = test_client.post(
-        '/api/download', json=[{'magnet': magnet, 'tmdb_id': '95792', 'season': '1'}]
+        '/api/download', json=[{'magnet': magnet, 'tmdb_id': '90000', 'season': '1'}]
     )
     assert res.status == '200 OK'
 
-    add_torrent.assert_called_with(magnet, 'tv_shows/Pocket Monsters/Season 1')
+    add_torrent.assert_called_with(magnet, 'tv_shows/Watchmen/Season 1')
 
     download = db.session.query(Download).first()
     assert download
@@ -175,7 +175,7 @@ def test_download_season_pack(test_client, responses, add_torrent):
     assert download.episode
     assert download.episode.season == 1
     assert download.episode.episode is None
-    assert download.episode.show_title == 'Pocket Monsters'
+    assert download.episode.show_title == 'Watchmen'
 
 
 def shallow(d: Dict):
