9a5833aa9a3b5737dffbb92e6b7418a9e60842b0
Elliana May <me@mause.me>
2020-04-25 21:56:49 +0800

    Switch to client from mause-rpc package

--- rarbg_local/transmission_proxy.py
+++ rarbg_local/transmission_proxy.py
@@ -1,12 +1,14 @@
 from functools import lru_cache
 from typing import TYPE_CHECKING
 
-from .client import get_client
+from mause_rpc.client import get_client
+
+from .config import get_parameters
 
 if TYPE_CHECKING:
     from .transmission import torrent_add, get_torrent
 else:
-    transmission = lru_cache()(get_client)
+    transmission = lru_cache()(lambda: get_client('rpc.server.queue', get_parameters()))
 
     def __getattr__(name):
         return lambda *args, **kwargs: getattr(transmission(), name)(*args, **kwargs)
