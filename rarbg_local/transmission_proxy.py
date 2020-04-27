c2d060fb2ae099591e95c9e01e977e3358a2cdb1
Elliana May <me@mause.me>
2020-04-27 12:06:17 +0800

    Use URLParameters

--- rarbg_local/transmission_proxy.py
+++ rarbg_local/transmission_proxy.py
@@ -1,14 +1,18 @@
+import os
 from functools import lru_cache
 from typing import TYPE_CHECKING
 
 from mause_rpc.client import get_client
-
-from .config import get_parameters
+from pika.configuration import URLParameters
 
 if TYPE_CHECKING:
     from .transmission import torrent_add, get_torrent
 else:
-    transmission = lru_cache()(lambda: get_client('rpc.server.queue', get_parameters()))
+    transmission = lru_cache()(
+        lambda: get_client(
+            'rpc.server.queue', URLParameters(os.environ['CLOUDAMQP_URL'])
+        )
+    )
 
     def __getattr__(name):
         return lambda *args, **kwargs: getattr(transmission(), name)(*args, **kwargs)
