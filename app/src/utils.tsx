52a5690e103fab102bf567f05a1899873c4c7173
Elliana May <me@mause.me>
2020-05-12 21:12:07 +0800

    Comment out import

--- app/src/utils.tsx
+++ app/src/utils.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import MaterialLink from '@material-ui/core/Link';
 import { Link } from 'react-router-dom';
 import { LocationDescriptor } from 'history';
-import axiosRetry from '@vtex/axios-concurrent-retry';
+// import axiosRetry from '@vtex/axios-concurrent-retry';
 import { TypographyTypeMap } from '@material-ui/core';
 import moxios from 'moxios';
 import { unstable_batchedUpdates } from 'react-dom';
