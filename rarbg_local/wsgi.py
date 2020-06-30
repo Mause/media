d0b7769f8016b514a3b9b0e7c8edb082e0d7baee
Elliana May <me@mause.me>
2020-06-30 13:04:22 +0800

    Enable apm

--- rarbg_local/wsgi.py
+++ rarbg_local/wsgi.py
@@ -26,6 +26,7 @@ if 'SENTRY_DSN' in os.environ:
         os.environ['SENTRY_DSN'],
         integrations=[FlaskIntegration(), SqlalchemyIntegration()],
         release=os.environ['HEROKU_SLUG_COMMIT'],
+        traces_sample_rate = 0.75
     )
 
 
