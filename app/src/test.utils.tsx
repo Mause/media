61d99e134facf19606b89a9541f0eea9465148cd
Elliana May <me@mause.me>
2020-08-18 20:30:05 +0800

    Integrate auth0

--- app/src/test.utils.tsx
+++ app/src/test.utils.tsx
@@ -1,10 +1,12 @@
+import React from 'react';
 import moxios from 'moxios';
 import { swrConfig } from './streaming';
 import { render } from '@testing-library/react';
 import { ReactElement } from 'react';
+import { Auth0Context, Auth0ContextInterface } from '@auth0/auth0-react';
 
 export function wait() {
-  return new Promise(resolve => moxios.wait(resolve));
+  return new Promise((resolve) => moxios.wait(resolve));
 }
 
 export async function mock<T>(path: string, response: T) {
@@ -14,7 +16,17 @@ export async function mock<T>(path: string, response: T) {
 }
 
 export function renderWithSWR(el: ReactElement) {
-  return render(swrConfig(() => el)());
+  const c: Auth0ContextInterface = {
+    isAuthenticated: true,
+    getAccessTokenSilently() {
+      return Promise.resolve('');
+    },
+  };
+  return render(
+    <Auth0Context.Provider value={c}>
+      {swrConfig(() => el)()}
+    </Auth0Context.Provider>,
+  );
 }
 
 export function usesMoxios() {
