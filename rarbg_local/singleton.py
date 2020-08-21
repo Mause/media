b10d08cb07b8865aa00c3626083dbfe6d451b4b4
Elliana May <me@mause.me>
2020-08-21 09:34:39 +0800

    Add singleton decorator

---
+++ rarbg_local/singleton.py
@@ -0,0 +1,39 @@
+from asyncio import iscoroutinefunction
+from typing import Callable, TypeVar
+
+from fastapi import FastAPI
+from fastapi.dependencies.utils import solve_dependencies
+from fastapi.requests import Request
+from fastapi.routing import get_dependant, run_endpoint_function
+
+T = TypeVar('T')
+
+
+async def get(app: FastAPI, func: Callable[..., T]) -> T:
+    dependant = get_dependant(call=func, path='')
+    request = Request({'type': 'http', 'query_string': '', 'headers': []}, None, None)
+
+    values, errors, *_ = await solve_dependencies(
+        request=request,
+        dependant=dependant,
+        dependency_overrides_provider=app.dependency_overrides,
+    )
+
+    assert not errors
+
+    return await run_endpoint_function(
+        dependant=dependant, values=values, is_coroutine=iscoroutinefunction(func)
+    )
+
+
+def singleton(app: FastAPI) -> Callable:
+    def decorator(func: Callable):
+        @app.on_event('startup')
+        async def startup():
+            value = await get(app, func)
+
+            app.dependency_overrides[func] = lambda: value
+
+        return func
+
+    return decorator
