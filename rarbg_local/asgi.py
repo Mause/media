f0dcf058d0d509c7e02ce1348a1c250e081cefdd
Elliana May <me@mause.me>
2020-08-23 14:39:05 +0800

    Actually read the sentry asgi documentation

--- rarbg_local/asgi.py
+++ rarbg_local/asgi.py
@@ -14,13 +14,14 @@ logger = logging.getLogger()
 if 'TIMBERIO_APIKEY' in os.environ:
     logger.addHandler(get_timber_handler())
 
-app = create_app()
-
 
 if 'SENTRY_DSN' in os.environ:
     sentry_sdk.init(
         os.environ['SENTRY_DSN'],
-        integrations=[SentryAsgiMiddleware(app), SqlalchemyIntegration()],
+        integrations=[SqlalchemyIntegration()],
         release=os.environ['HEROKU_SLUG_COMMIT'],
         traces_sample_rate=0.75,
     )
+
+app = create_app()
+app = SentryAsgiMiddleware(app)
