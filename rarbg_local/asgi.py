dbc80050eb0635e09d280b89d8e54e751352dadc
Elliana May <me@mause.me>
2020-08-23 13:18:46 +0800

    Configure logging and sentry for asgi

--- rarbg_local/asgi.py
+++ rarbg_local/asgi.py
@@ -1,3 +1,27 @@
+import logging
+import os
+
+import sentry_sdk
+from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
+from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
+
+from .logger import get_timber_handler
 from .new import create_app
+
+logger = logging.getLogger()
+
+
+if 'TIMBERIO_APIKEY' in os.environ:
+    logger.addHandler(get_timber_handler())
+
+
+if 'SENTRY_DSN' in os.environ:
+    sentry_sdk.init(
+        os.environ['SENTRY_DSN'],
+        integrations=[SentryAsgiMiddleware(), SqlalchemyIntegration()],
+        release=os.environ['HEROKU_SLUG_COMMIT'],
+        traces_sample_rate=0.75,
+    )
+
 
 app = create_app()
