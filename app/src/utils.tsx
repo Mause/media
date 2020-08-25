080951101ed82086421b60d1286d550851f10ec4
Elliana May <me@mause.me>
2020-08-25 18:08:57 +0800

    Remove double parse

--- app/src/utils.tsx
+++ app/src/utils.tsx
@@ -43,7 +43,7 @@ export function subscribe<T>(
         end();
       }
     } else {
-      callback(JSON.parse(data));
+      callback(data);
     }
   };
   es.addEventListener('message', internal_callback);
