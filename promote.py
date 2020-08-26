fd95c63b36fd24bccdfe0e953c211acd3fbbbcdd
Elliana May <me@mause.me>
2020-08-26 18:16:05 +0800

    Strip token

--- promote.py
+++ promote.py
@@ -4,7 +4,7 @@ import requests
 
 from follower import follow
 
-token = open('token.txt').read()
+token = open('token.txt').read().strip()
 
 r = requests.post(
     "https://kolkrabbi.heroku.com/apps/c0ac59e0-1586-49e4-b326-9ddfe8c6656f/github/push",
