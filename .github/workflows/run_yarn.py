c40b0898a512791b745e90479ff76c060d5b4950
Elliana May <me@mause.me>
2020-09-01 16:32:09 +0800

    Fix cwd

--- .github/workflows/run_yarn.py
+++ .github/workflows/run_yarn.py
@@ -12,4 +12,4 @@ if '/' in branch_name:
 branch_name = branch_name.rsplit('-', 1)[0]
 print(branch_name)
 
-check_call(['yarn', 'up', branch_name])
+check_call(['yarn', 'up', branch_name], cwd='app')
