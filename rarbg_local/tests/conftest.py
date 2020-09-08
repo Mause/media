f12bcd7bb7cc071650cdc34af5f4fbd5b143b865
Elliana May <me@mause.me>
2020-09-08 20:30:05 +0800

    Fix typo

--- rarbg_local/tests/conftest.py
+++ rarbg_local/tests/conftest.py
@@ -53,7 +53,7 @@ def session(fastapi_app):
     Session = get_event_loop().run_until_complete(get(fastapi_app, get_session_local))
     assert hasattr(Session, 'kw'), Session
     engine = Session.kw['bind']
-    assert 'sqlite' in repr(engine.async_engine), repr(engine.async_engine)
+    assert 'sqlite' in repr(engine.sync_engine), repr(engine.sync_engine)
     Base.metadata.create_all(engine.sync_engine)
 
     with Session() as session:
