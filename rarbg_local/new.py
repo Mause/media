b887b349250d1fbd7c770390403a5699108725bc
Elliana May <me@mause.me>
2020-09-10 07:33:32 +0800

    Fix monitor_post

--- rarbg_local/new.py
+++ rarbg_local/new.py
@@ -418,15 +418,14 @@ async def monitor_post(
     session: AsyncSession = Depends(get_db),
 ):
     media = validate_id(monitor.type, monitor.tmdb_id)
+
     c = (
-        (
-            await session.execute(
-                select(Monitor).filter_by(tmdb_id=monitor.tmdb_id, type=monitor.type)
-            )
+        await session.execute(
+            select(Monitor)
+            .where(Monitor.tmdb_id == monitor.tmdb_id)
+            .where(Monitor.type == monitor.type)
         )
-        .scalars()
-        .one_or_none()
-    )
+    ).scalar_one_or_none()
     if not c:
         c = Monitor(
             tmdb_id=monitor.tmdb_id, added_by=user, type=monitor.type, title=media
