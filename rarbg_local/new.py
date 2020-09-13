480307a585c5bdb34dedefb6acc88d5f5f6bba0c
Elliana May <me@mause.me>
2020-09-13 11:37:01 +0800

    Cleanup

--- rarbg_local/new.py
+++ rarbg_local/new.py
@@ -9,7 +9,7 @@ from urllib.parse import urlencode
 
 from fastapi import APIRouter, Depends, FastAPI, HTTPException, Security, WebSocket
 from fastapi.requests import Request
-from fastapi.responses import StreamingResponse
+from fastapi.responses import RedirectResponse, StreamingResponse
 from fastapi.security import (
     HTTPAuthorizationCredentials,
     HTTPBearer,
@@ -35,7 +35,16 @@ from .db import (
     get_movies,
 )
 from .health import health
-from .main import get_imdb_in_plex, get_plex
+from .main import (
+    add_single,
+    extract_marker,
+    get_imdb_in_plex,
+    get_keyed_torrents,
+    get_plex,
+    groupby,
+    normalise,
+    resolve_series,
+)
 from .models import (
     DownloadAllResponse,
     DownloadPost,
@@ -217,7 +226,6 @@ def stream(
     '/select/{tmdb_id}/season/{season}/download_all', response_model=DownloadAllResponse
 )
 async def select(tmdb_id: int, season: int):
-    from .main import extract_marker, groupby, normalise
 
     results = search_for_tv(get_tv_imdb_id(tmdb_id), int(tmdb_id), int(season))
 
@@ -257,7 +265,6 @@ async def download_post(
     added_by: User = Depends(get_current_user),
     session: Session = Depends(get_db),
 ) -> List[Union[MovieDetails, EpisodeDetails]]:
-    from .main import add_single
 
     results: List[Union[MovieDetails, EpisodeDetails]] = []
 
@@ -318,14 +325,12 @@ async def download_post(
 
 @api.get('/index', response_model=IndexResponse)
 async def index(session: Session = Depends(get_db)):
-    from .main import resolve_series
 
     return IndexResponse(series=resolve_series(session), movies=get_movies(session))
 
 
 @api.get('/stats', response_model=List[StatsResponse])
 async def stats(session: Session = Depends(get_db)):
-    from .main import groupby
 
     keys = Download.added_by_id, Download.type
     query = session.query(*keys, func.count(name='count')).group_by(*keys)
@@ -346,7 +351,6 @@ def movie(tmdb_id: int):
 
 @api.get('/torrents', response_model=Dict[str, InnerTorrent])
 async def torrents():
-    from .main import get_keyed_torrents
 
     return get_keyed_torrents()
 
@@ -461,10 +465,6 @@ async def static(
     return await static_files.get_response(filename, request.scope)
 
 
-def redirect(url):
-    raise HTTPException(301, headers={'Location': url})
-
-
 @root.get('/redirect/plex/{tmdb_id}')
 def redirect_to_plex(tmdb_id: str):
     dat = get_imdb_in_plex(tmdb_id)
@@ -473,25 +473,27 @@ def redirect_to_plex(tmdb_id: str):
 
     server_id = get_plex().machineIdentifier
 
-    return redirect(
+    return RedirectResponse(
         f'https://app.plex.tv/desktop#!/server/{server_id}/details?'
         + urlencode({'key': f'/library/metadata/{dat.ratingKey}'})
     )
 
 
-@root.get('/redirect/{type_}/{ident}')
-@root.get('/redirect/{type_}/{ident}/{season}/{episode}')
-def redirect_to_imdb(type_: str, ident: str, season: str = None, episode: str = None):
+@root.get('/redirect/{type_}/{tmdb_id}')
+@root.get('/redirect/{type_}/{tmdb_id}/{season}/{episode}')
+def redirect_to_imdb(
+    type_: MediaType, tmdb_id: int, season: int = None, episode: int = None
+):
     if type_ == 'movie':
-        imdb_id = get_movie_imdb_id(ident)
+        imdb_id = get_movie_imdb_id(tmdb_id)
     elif season:
         imdb_id = get_json(
-            f'tv/{ident}/season/{season}/episode/{episode}/external_ids'
+            f'tv/{tmdb_id}/season/{season}/episode/{episode}/external_ids'
         )['imdb_id']
     else:
-        imdb_id = get_tv_imdb_id(ident)
+        imdb_id = get_tv_imdb_id(tmdb_id)
 
-    return redirect(f'https://www.imdb.com/title/{imdb_id}')
+    return RedirectResponse(f'https://www.imdb.com/title/{imdb_id}')
 
 
 api.include_router(tv_ns, prefix='/tv')
