68e2dfbb8f299886a578dc1b19a32f979fcf7180
Elliana May <me@mause.me>
2020-09-27 22:05:32 +0800

    Update monitor.py to display worker_pid

--- tools/monitor.py
+++ tools/monitor.py
@@ -16,47 +16,48 @@ def get_connections():
     response = requests.post(
         "https://data-api.heroku.com/graphql",
         json={
-            "query": "{ postgres(addon_uuid: $addon) { connections } } ",
-            "variables": {"addon": "8d550da5-e054-47c2-8a5c-1635d27281ee"},
+            "query": (
+                "{ postgres(addon_uuid: \"8d550da5-e054-47c2-8a5c-1635d27281ee\") {"
+                " connections } } "
+            ),
         },
         headers={'authorization': "Bearer " + open('token.txt').read().strip()},
     ).json()
 
     if 'errors' in response:
+        print(response['errors'])
         return '?'
 
     return int(response['data']['postgres']['connections'].split('/')[0])
 
 
-def get_pool(key):
-    def getter():
-        try:
-            data = requests.get(
-                'https://media.mause.me/api/diagnostics/pool',
-                headers={
-                    'authorization': (
-                        'Bearer ' + open('media_token.txt').read().strip()
-                    )
-                },
-                timeout=1,
-            ).json()
-        except (ReadTimeout, ConnectionError):
-            return '?'
+def get_pool():
+    try:
+        data = requests.get(
+            'https://media.mause.me/api/diagnostics/pool',
+            headers={
+                'authorization': ('Bearer ' + open('media_token.txt').read().strip())
+            },
+            timeout=1,
+        ).json()
+    except (ReadTimeout, ConnectionError):
+        return {}
 
-        return data[key]
-
-    return getter
+    return data
 
 
 def main():
+    data = {}
+
     graphs = [
         Graph('connections', get_connections),
         *[
-            Graph(key, get_pool(key))
+            Graph(key, lambda key=key: data.pop(key, '?'))
             for key in ('size', 'checkedin', 'overflow', 'checkedout')
         ],
     ]
     while True:
+        data.update(get_pool())
         for g in graphs:
             g.update()
 
@@ -64,7 +65,11 @@ def main():
             RenderGroup(
                 CLEAR_SCREEN,
                 Columns(graphs),
-                Text(datetime.now().isoformat(), style='blue'),
+                Text(
+                    'timestamp: [blue]{}[/], worker: [blue]{}[/]'.format(
+                        datetime.now().isoformat(), data.pop('worker_pid', '?')
+                    )
+                ),
             )
         )
 
