a2486e1d20a48c94164f22afa49826f4d0775901
Elliana May <me@mause.me>
2025-04-21 17:35:07 +0800

    disable unused import

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { appUpdated } from './serviceWorkerCallback';
+// import { appUpdated } from './serviceWorkerCallback';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
