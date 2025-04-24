43225aeed33e13081187e3097b75596f1c65495e
Elliana May <me@mause.me>
2025-04-24 13:04:42 +0800

    hax

--- alembic/env.py
+++ alembic/env.py
@@ -60,11 +60,16 @@ def run_migrations_offline():
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url,
+        #        url=url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        dialect_name='postgresql',
     )
+
+    from sqlalchemy.engine.strategies import MockEngineStrategy  # .MockConnection
+
+    MockEngineStrategy.MockConnection.close = lambda self: None
 
     with context.begin_transaction():
         context.run_migrations()
