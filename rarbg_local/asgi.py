212b0219195771393c126567b84ceb4e3dee55fa
Elliana May <me@mause.me>
2025-04-27 14:24:46 +0800

    remove timber references

--- rarbg_local/asgi.py
+++ rarbg_local/asgi.py
@@ -1,14 +1,9 @@
 import logging
 import os
 
-from .logger import get_timber_handler
 from .new import create_app
 
 logger = logging.getLogger()
-
-
-if 'TIMBERIO_APIKEY' in os.environ:
-    logger.addHandler(get_timber_handler())
 
 
 if 'SENTRY_DSN' in os.environ:
