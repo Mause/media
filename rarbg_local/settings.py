1337966b21ccfcc2e2b141ccfc9019e78b90e7cd
Elliana May <me@mause.me>
2025-04-28 22:35:08 +0800

    extract settings.py

---
+++ rarbg_local/settings.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from typing import Optional
+
+from pydantic import BaseSettings, SecretStr
+
+from .singleton import singleton
+
+
+class Settings(BaseSettings):
+    root = Path(__file__).parent.parent.absolute()
+    database_url = f"sqlite:///{root}/db.db"
+    static_resources_path = root / 'app/build'
+    plex_username: Optional[str] = None
+    plex_password: Optional[SecretStr] = None
+
+
+@singleton
+async def get_settings():
+    return Settings()
