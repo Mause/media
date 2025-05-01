126857bbea57cf56d4b92f13aebfa27195d1c0b5
Elliana May <me@mause.me>
2025-05-01 20:38:00 +0800

    implement movie support for pirate bay

--- rarbg_local/providers/__init__.py
+++ rarbg_local/providers/__init__.py
@@ -271,7 +271,24 @@ class PirateBayProvider(TvProvider, MovieProvider):
     async def search_for_movie(
         self, imdb_id: str, tmdb_id: int
     ) -> AsyncGenerator[ITorrent, None]:
-        pass
+        async with (
+            aiohttp.ClientSession() as session,
+            await session.get(self.root + '/q.php', params={'q': imdb_id}) as resp,
+        ):
+            data = await resp.json()
+
+            if len(data) == 1 and data[0]['name'] == 'No results returned':
+                return
+
+            for item in data:
+                yield ITorrent(
+                    id=item['id'],
+                    source=ProviderSource.PIRATEBAY,
+                    title=item['name'],
+                    seeders=item['seeders'],
+                    download=item['info_hash'],
+                    category=movie_convert(item['category']),
+                )
 
 
 PROVIDERS = [
