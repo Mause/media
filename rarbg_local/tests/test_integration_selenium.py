dc8dd95563c46f1fb439824ac35e69c67e8c70f2
Elliana May <me@mause.me>
2025-05-02 17:34:36 +0800

    fix long lines

--- rarbg_local/tests/test_integration_selenium.py
+++ rarbg_local/tests/test_integration_selenium.py
@@ -98,7 +98,14 @@ def test_simple(server_url: str, selenium: Chrome) -> None:
         + '/download/series?'
         + urlencode(
             {
-                'magnet': 'magnet:?xt=urn:btih:0a49edcbe6cfca62a7b8b24a7f60094b697aa2e9&dn=Chernobyl.S01E01.iNTERNAL.1080p.WEB.H264-EDHD%5Brartv%5D&tr=http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2710&tr=udp%3A%2F%2F9.rarbg.to%3A2710&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce',
+                'magnet': (
+                    'magnet:?xt=urn:btih:0a49edcbe6cfca62a7b8b24a7f60094b697aa2e9'
+                    '&dn=Chernobyl.S01E01.iNTERNAL.1080p.WEB.H264-EDHD%5Brartv%5D'
+                    '&tr=http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce'
+                    '&tr=udp%3A%2F%2F9.rarbg.me%3A2710'
+                    '&tr=udp%3A%2F%2F9.rarbg.to%3A2710'
+                    '&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce'
+                ),
                 'imdb_id': 'tt7366338',
                 'titles': '1:23:45',
                 'season': '1',
@@ -162,7 +169,14 @@ def test_movie(server_url: str, selenium: Chrome) -> None:
         + '/download/movie?'
         + urlencode(
             {
-                'magnet': 'magnet:?xt=urn:btih:18560491ab3e461ba04fcefda0c49cd1633be12a&dn=Pokemon.Detective.Pikachu.2019.1080p.BluRay.x264-AAA&tr=http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2710&tr=udp%3A%2F%2F9.rarbg.to%3A2710&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce',
+                'magnet': (
+                    'magnet:?xt=urn:btih:18560491ab3e461ba04fcefda0c49cd1633be12a'
+                    '&dn=Pokemon.Detective.Pikachu.2019.1080p.BluRay.x264-AAA'
+                    '&tr=http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce'
+                    '&tr=udp%3A%2F%2F9.rarbg.me%3A2710'
+                    '&tr=udp%3A%2F%2F9.rarbg.to%3A2710&'
+                    'tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce'
+                ),
                 'imdb_id': 'tt5884052',
                 'titles': 'Pokémon Detective Pikachu',
             }
