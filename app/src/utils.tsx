10e61591fc6c9048e71c4ac329032679a9424d35
Elliana May <me@mause.me>
2025-05-04 22:50:59 +0800

    remove reference to history

--- app/src/utils.tsx
+++ app/src/utils.tsx
@@ -9,16 +9,15 @@ import { TypographyTypeMap } from '@material-ui/core';
 import moxios from 'moxios';
 import { useAuth0 } from '@auth0/auth0-react';
 import { FetchEventTarget } from './fetch_stream';
-import { To, State } from 'history';
 
 // axiosRetry(Axios, { retries: 3 });
 
-export function MLink(props: {
-  children: React.ReactNode;
-  to: To;
-  state?: State;
-  color?: TypographyTypeMap['props']['color'];
-}): ReactElement {
+export function MLink(
+  props: {
+    children: React.ReactNode;
+    color?: TypographyTypeMap['props']['color'];
+  } & Pick<Parameters<typeof Link>[0], 'to' | 'state'>,
+): ReactElement {
   return <MaterialLink component={Link} {...props} />;
 }
 
