bd384b8f8e2bb4e72815b4115244beab9d2e592c
Elliana May <me@mause.me>
2025-05-05 10:49:33 +0800

    remove unused stubs

--- rarbg_local/tests/test_auth.py
+++ rarbg_local/tests/test_auth.py
@@ -4,7 +4,6 @@ from pytest import mark
 from ..auth import get_my_jwkaas
 from ..models import UserSchema
 from ..new import get_current_user, security
-from .conftest import add_json
 
 
 @mark.asyncio
@@ -15,13 +14,6 @@ async def test_auth(responses, user, fastapi_app, test_client):
     from jwt.api_jwt import PyJWT
 
     # Arrange
-    add_json(
-        responses,
-        'GET',
-        'https://mause.au.auth0.com/userinfo',
-        {'email': user.email},
-    )
-
     KID = 'kid'
     private_key = rsa.generate_private_key(
         public_exponent=65537, key_size=2048, backend=default_backend()
