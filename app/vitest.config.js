0b59fb72a40259cdc68ae6a66e3843669e52f0e8
Elliana May <me@mause.me>
2025-05-07 23:57:42 +0800

    configure setupFiles.ts

--- app/vitest.config.js
+++ app/vitest.config.js
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom', // Use jsdom for browser-like tests
+    setupFiles: ['./src/setupTests.ts'],
     coverage: {
       reporter: ['text', 'json', 'html'], // Optional: Add coverage reports
     },
