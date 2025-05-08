b8b70b8311d88455105f520371a8cbe757008b73
Elliana May <me@mause.me>
2025-05-08 15:28:28 +0800

    add sentry logging

--- rarbg_local/asgi.py
+++ rarbg_local/asgi.py
@@ -3,22 +3,27 @@ import os
 
 from .new import create_app
 
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
 
 
 if 'SENTRY_DSN' in os.environ:
     import sentry_sdk
-    from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
     from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
+
+    logger.info('Configuring Sentry')
 
     sentry_sdk.init(
         os.environ['SENTRY_DSN'],
         integrations=[SqlalchemyIntegration()],
-        release=os.environ['HEROKU_SLUG_COMMIT'],
+        release=os.environ.get('HEROKU_SLUG_COMMIT', os.environ.get('RAILWAY_GIT_COMMIT_SHA')),
         traces_sample_rate=1,
     )
+else:
+    logger.warn('Not configuring sentry')
 
 app = create_app()
 
 if 'SENTRY_DSN' in os.environ:
+    from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
+
     app = SentryAsgiMiddleware(app)
