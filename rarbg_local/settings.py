fd7a8d7d3ca41f7a9e52497a2df90f336741013d
Elliana May <me@mause.me>
2025-05-08 22:31:02 +0800

    add missing field annotations

--- rarbg_local/settings.py
+++ rarbg_local/settings.py
@@ -7,9 +7,9 @@ from .singleton import singleton
 
 
 class Settings(BaseSettings):
-    root = Path(__file__).parent.parent.absolute()
-    database_url = f"sqlite:///{root}/db.db"
-    static_resources_path = root / 'app/build'
+    root: Path = Path(__file__).parent.parent.absolute()
+    database_url: str = f"sqlite:///{root}/db.db"
+    static_resources_path: Path = root / 'app/build'
     plex_token: SecretStr
 
 
