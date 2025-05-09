f559e0dd3ebbd0ea087de1f635be9ae99802520b
Elliana May <me@mause.me>
2025-05-09 10:21:23 +0800

    add LRUCache protocol

--- rarbg_local/utils.py
+++ rarbg_local/utils.py
@@ -1,11 +1,17 @@
 from functools import lru_cache as _lru_cache
-from typing import Optional, Set, TypeVar
+from typing import Optional, Protocol, Set, TypeVar
 
 from asyncache import cached as _cached
 from cachetools.func import ttl_cache as _ttl_cache
 
+
+class LRUCache(Protocol):
+    def cache_clear(self):
+        pass
+
+
 T = TypeVar('T')
-_caches: Set[_lru_cache] = set()
+_caches: Set[LRUCache] = set()
 
 
 def lru_cache(*args, **kwargs):
