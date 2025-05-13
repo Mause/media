90a5f4f43e333d67c455d356e9a6c85b5dab1abc
Elliana May <me@mause.me>
2025-05-14 00:11:25 +0800

    steal create_monitored_task

--- rarbg_local/utils.py
+++ rarbg_local/utils.py
@@ -1,4 +1,8 @@
+import asyncio
+from collections.abc import Callable, Coroutine
+from dataclasses import dataclass
 from functools import lru_cache as _lru_cache
+from functools import partial
 from typing import Protocol, TypeVar
 
 from asyncache import cached as _cached
@@ -60,3 +64,30 @@ def precondition(res: T | None, message: str) -> T:
     if not res:
         raise AssertionError(message)
     return res
+
+
+@dataclass
+class Message:
+    event: str
+    reason: str
+    task: asyncio.Task
+
+
+def _callback(send, fut):
+    try:
+        fut.result()
+    except asyncio.CancelledError:
+        send(Message("exit", "killed", fut))
+        raise
+    except Exception as e:
+        send(Message("err", e, fut))
+    else:
+        send(Message("exit", "normal", fut))
+
+
+def create_monitored_task(
+    coro: Coroutine[None, None, T], send: Callable
+) -> asyncio.Future[T]:
+    future = asyncio.ensure_future(coro)
+    future.add_done_callback(partial(_callback, send))
+    return future
