16e93aae5efd6c60b95df1e47c887664c0cc938f
Elliana May <me@mause.me>
2025-05-14 00:11:25 +0800

    steal create_monitored_task

---
+++ rarbg_local/tests/test_utils.py
@@ -0,0 +1,21 @@
+import asyncio
+
+from pytest import mark
+
+from ..utils import Message, create_monitored_task
+
+
+async def coro(queue: asyncio.Queue[int]):
+    await queue.put(1)
+
+
+@mark.asyncio
+async def test_create_monitored_task():
+    output_queue = asyncio.Queue[int | Message]()
+
+    create_monitored_task(coro(output_queue), output_queue.put_nowait)
+
+    assert await output_queue.get() == 1
+    message = await output_queue.get()
+    assert isinstance(message, Message)
+    assert message.event == 'exit'
