08702ee2d35df38d980915d3df7e53fc58d66e15
Elliana May <me@mause.me>
2025-05-15 18:48:48 +0800

    add cast

--- rarbg_local/tmdb.py
+++ rarbg_local/tmdb.py
@@ -2,7 +2,7 @@ import os
 from datetime import date
 from enum import Enum
 from itertools import chain
-from typing import Literal
+from typing import Literal, cast
 
 import aiohttp
 import aiohttp.web_exceptions
@@ -33,7 +33,7 @@ def try_(dic: dict[str, str], *keys: str) -> str | None:
     backoff.fibo,
     aiohttp.web_exceptions.HTTPException,
     max_tries=5,
-    giveup=lambda e: getattr(e.response, 'status_code', None) != 429,
+    giveup=lambda e: getattr(cast(aiohttp.web_exceptions.HTTPException, e).response, 'status_code', None) != 429,
 )
 async def get_json(path, **kwargs):
     async with aiohttp.ClientSession(
