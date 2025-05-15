d7b0c5785484c72ddc1fe0540e2433d753648b92
Elliana May <me@mause.me>
2025-05-15 16:22:01 +0800

    ignore type errors

--- app/src/test.utils.tsx
+++ app/src/test.utils.tsx
@@ -47,9 +47,11 @@ export function renderWithSWR(el: ReactElement) {
 
 export function usesMoxios() {
   beforeEach(() => {
+    // @ts-expect-error
     moxios.install(axios);
   });
   afterEach(() => {
+    // @ts-expect-error
     moxios.uninstall(axios);
   });
 }
