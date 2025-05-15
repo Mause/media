7c8f3f79116366fdf46cf66381f24001d3cd3c32
Elliana May <me@mause.me>
2025-05-15 18:09:59 +0800

    enable more sentry features

--- rarbg_local/asgi.py
+++ rarbg_local/asgi.py
@@ -18,7 +18,18 @@ if 'SENTRY_DSN' in os.environ:
         release=os.environ.get(
             'HEROKU_SLUG_COMMIT', os.environ.get('RAILWAY_GIT_COMMIT_SHA')
         ),
+        # Add data like request headers and IP for users, if applicable;
+        # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+        send_default_pii=True,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for tracing.
         traces_sample_rate=1,
+        # To collect profiles for all profile sessions,
+        # set `profile_session_sample_rate` to 1.0.
+        profile_session_sample_rate=1.0,
+        # Profiles will be automatically collected while
+        # there is an active span.
+        profile_lifecycle="trace",
     )
 else:
     logger.warning('Not configuring sentry')
