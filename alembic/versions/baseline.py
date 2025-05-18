6d412720717216cc4a0c9883793b592eb681c519
Elliana May <me@mause.me>
2025-05-18 17:55:23 +0800

    support sqlite collations in baseline script

--- alembic/versions/baseline.py
+++ alembic/versions/baseline.py
@@ -8,6 +8,7 @@ Create Date: 2025-05-25
 import sqlalchemy as sa
 
 from alembic import op
+from common import get_driver
 
 # revision identifiers, used by Alembic.
 revision = "baseline"
@@ -18,9 +19,17 @@ depends_on = None
 
 def upgrade():
     conn = op.get_bind()
-    conn.execute(sa.text("CREATE COLLATION \"en_AU\" (LOCALE = 'en_AU.utf8')"))
+    if get_driver() == 'sqlite':
+        conn.connection.driver_connection.create_collation(
+            "en_AU", lambda a, b: 0 if a.lower() == b.lower() else -1
+        )
+    else:
+        conn.execute(sa.text("CREATE COLLATION \"en_AU\" (LOCALE = 'en_AU.utf8')"))
 
 
 def downgrade():
     conn = op.get_bind()
-    conn.execute(sa.text("DROP COLLATION IF EXISTS \"en_AU\""))
+    if get_driver() == 'sqlite':
+        conn.connection.driver_connection.drop_collation("en_AU")
+    else:
+        conn.execute(sa.text("DROP COLLATION IF EXISTS \"en_AU\""))
