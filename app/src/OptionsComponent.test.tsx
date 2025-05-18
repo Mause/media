e06725050940a6a0f178c23ae8e74c5607ca076f
Elliana May <me@mause.me>
2025-05-19 00:12:27 +0800

    mock stream directly

--- app/src/OptionsComponent.test.tsx
+++ app/src/OptionsComponent.test.tsx
@@ -133,30 +133,16 @@ describe('OptionsComponent', () => {
 
     expect(container).toMatchSnapshot();
 
-    const torrent: ITorrent = {
-      source: 'rarbg',
-      title: 'title',
-      seeders: 5,
-      download: 'magnet:...',
-      category: 'x264/1080',
-      episode_info: { seasonnum: 1, epnum: 1 },
-    };
-
-    expect(sources).toHaveLength(3);
-
-    let i = 0;
-    const source_names = ['RARBG', 'HORRIBLESUBS', 'KICKASS'];
-    await act(async () => {
-      for (const source of sources) {
-        source!.ls!({
-          data: JSON.stringify({
-            ...torrent,
-            source: source_names[i],
-            title: 'title ' + i++,
-          }),
-        });
-      }
-    });
+    mockFetchStream([
+      {
+        source: 'rarbg',
+        title: 'title',
+        seeders: 5,
+        download: 'magnet:...',
+        category: 'x264/1080',
+        episode_info: { seasonnum: 1, epnum: 1 },
+      },
+    ]);
 
     expect(container).toMatchSnapshot();
 
