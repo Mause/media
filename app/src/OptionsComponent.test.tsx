8a18ffb1adff012a83c77d1f270cc8ea954e7ced
Elliana May <me@mause.me>
2025-05-18 11:22:42 +0800

    fixup! logging

--- app/src/OptionsComponent.test.tsx
+++ app/src/OptionsComponent.test.tsx
@@ -13,6 +13,7 @@ import { vi } from 'vitest';
 
 vi.mock('@auth0/auth0-react', async (importOriginal) => {
   const original = await importOriginal();
+  console.log('mocking auth0');
   const useAuth0 = vi.fn();
 
   useAuth0.mockReturnValue({
