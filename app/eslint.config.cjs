d82ec5793bdb896056da0a392cb24b98fc58b0fe
Elliana May <me@mause.me>
2025-05-19 14:40:00 +0000

    swap to eslint 9 dep and config

---
+++ app/eslint.config.cjs
@@ -0,0 +1,15 @@
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+
+module.exports = tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
+);
