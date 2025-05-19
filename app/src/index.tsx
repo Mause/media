3ff390d7377671e43c9e6e5c369140de60d591ff
Elliana May <me@mause.me>
2025-05-19 14:33:27 +0000

    remove old service worker

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { createRoot } from 'react-dom/client';
-// import { appUpdated } from './serviceWorkerCallback';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 import {
@@ -47,9 +45,3 @@ const rootEl = (
   </Auth0Provider>
 );
 root.render(<StrictMode>{rootEl}</StrictMode>);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-// serviceWorker.register({ onUpdate: appUpdated });
