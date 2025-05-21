8274edb1a987f2d4eba0731020e3add6f0c832e4
Elliana May <me@mause.me>
2025-05-21 11:09:21 +0800

    extract monitor_ns

--- rarbg_local/new.py
+++ rarbg_local/new.py
@@ -30,8 +30,6 @@ from .auth import auth_hook, get_my_jwkaas
 from .db import (
     Download,
     EpisodeDetails,
-    Monitor,
-    MonitorMediaType,
     MovieDetails,
     User,
     get_db,
@@ -55,8 +53,6 @@ from .models import (
     InnerTorrent,
     ITorrent,
     MediaType,
-    MonitorGet,
-    MonitorPost,
     MovieDetailsSchema,
     MovieResponse,
     ProviderSource,
@@ -65,6 +61,7 @@ from .models import (
     TvResponse,
     TvSeasonResponse,
 )
+from .monitor import monitor_ns
 from .plex import get_imdb_in_plex, get_plex
 from .providers import (
     get_providers,
@@ -337,57 +334,6 @@ async def torrents():
 async def search(query: str):
     return await search_themoviedb(query)
 
-
-monitor_ns = APIRouter(tags=['monitor'])
-
-
-@monitor_ns.get('', response_model=list[MonitorGet])
-async def monitor_get(
-    user: Annotated[User, security], session: Session = Depends(get_db)
-):
-    return session.query(Monitor).all()
-
-
-@monitor_ns.delete('/{monitor_id}')
-async def monitor_delete(monitor_id: int, session: Session = Depends(get_db)):
-    safe_delete(session, Monitor, monitor_id)
-
-    return {}
-
-
-async def validate_id(type: MonitorMediaType, tmdb_id: TmdbId) -> str:
-    try:
-        return (
-            (await get_movie(tmdb_id)).title
-            if type == MonitorMediaType.MOVIE
-            else (await get_tv(tmdb_id)).name
-        )
-    except HTTPError as e:
-        if e.response.status_code == 404:
-            raise HTTPException(422, f'{type.name} not found: {tmdb_id}')
-        else:
-            raise
-
-
-@monitor_ns.post('', response_model=MonitorGet, status_code=201)
-async def monitor_post(
-    monitor: MonitorPost,
-    user: Annotated[User, security],
-    session: Session = Depends(get_db),
-):
-    media = await validate_id(monitor.type, monitor.tmdb_id)
-    c = (
-        session.query(Monitor)
-        .filter_by(tmdb_id=monitor.tmdb_id, type=monitor.type)
-        .one_or_none()
-    )
-    if not c:
-        c = Monitor(
-            tmdb_id=monitor.tmdb_id, added_by=user, type=monitor.type, title=media
-        )
-        session.add(c)
-        session.commit()
-    return c
 
 
 tv_ns = APIRouter(tags=['tv'])
