e546e37b6a73ea541ff54c7f944e364c00151f3e
Elliana May <me@mause.me>
2025-05-31 10:17:33 +0800

    actually call column visitor

--- fix_pandas.py
+++ fix_pandas.py
@@ -290,6 +290,16 @@ if __name__ == '__main__':
         [
             'codemod',
             '-x',
+            'fix_pandas.ColumnVisitor',
+            'rarbg_local',
+        ],
+    )
+
+    libcst.tool.main(
+        '',
+        [
+            'codemod',
+            '-x',
             'fix_pandas.FixPandasVisitor',
             'rarbg_local',
         ],
