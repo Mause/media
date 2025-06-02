1d3afd5c8d53b5bb80a6fc066bf2872f6e75cca1
Elliana May <me@mause.me>
2025-06-03 03:01:22 +0800

    tweak return type

--- rarbg_local/tmdb.py
+++ rarbg_local/tmdb.py
@@ -53,7 +53,7 @@ async def get_json(path: str, hydrate: type[TBaseModel], **kwargs: Any) -> TBase
 class EmptyStringAsNoneModel(BaseModel):
     @model_validator(mode="before")
     @classmethod
-    def empty_str_to_none(cls, data: T) -> T:
+    def empty_str_to_none(cls, data: T) -> T | dict:
         if isinstance(data, dict):
             return {k: (None if v == '' else v) for k, v in data.items()}
         return data
