12d9e77d47340ce110830eb0b0f2d874ff9e7a1c
Elliana May <me@mause.me>
2025-06-07 01:02:34 +0800

    add polyfill package

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,14 +1,26 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import inject from '@rollup/plugin-inject';
+
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      overrides: {
+        fs: 'browserfs/dist/shims/fs',
+      },
+    }),
+    inject({
+      BrowserFS: '/src/bfs.js',
+    }),
+  ],
   envPrefix: 'REACT_APP_',
   build: {
     // to output your build into build dir the same as Webpack
     outDir: 'build',
     sourcemap: true,
-    rollupOptions: { external: ['util', 'fs'] },
   },
   server: {
     open: true,
