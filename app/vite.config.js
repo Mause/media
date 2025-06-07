2007269a23059b817d7e995d8375607958287285
Elliana May <me@mause.me>
2025-06-07 15:33:39 +0800

    use cert locally

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   envPrefix: 'REACT_APP_',
   build: {
     // to output your build into build dir the same as Webpack
