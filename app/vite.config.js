ba19b1289c15f041b792bf5507b3b1ef628e7f51
Elliana May <me@mause.me>
2025-06-13 23:09:34 +0800

    remove moxios runtime dep

--- app/vite.config.js
+++ app/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     basicSsl(),
-    bundlesize({ limits: [{ name: 'assets/index-*.js', limit: '914 kB' }] }),
+    bundlesize({ limits: [{ name: 'assets/index-*.js', limit: '868.9 kB' }] }),
   ],
   envPrefix: 'REACT_APP_',
   build: {
