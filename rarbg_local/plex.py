7f1deaba1872ff26ef087d6a34b296ad8df78085
Elliana May <me@mause.me>
2025-06-21 17:58:29 +0800

    type plex.py

--- rarbg_local/plex.py
+++ rarbg_local/plex.py
@@ -1,17 +1,19 @@
+from typing import Annotated
+
 from fastapi import Depends
 from plexapi.media import Media
 from plexapi.myplex import MyPlexAccount
 from plexapi.server import PlexServer
 from sentry_sdk import trace
 
-from .settings import get_settings
+from .settings import Settings, get_settings
 from .singleton import singleton
 from .types import ImdbId
 
 
 @singleton
 @trace
-def get_plex(settings=Depends(get_settings)) -> PlexServer:
+def get_plex(settings: Annotated[Settings, Depends(get_settings)]) -> PlexServer:
     acct = MyPlexAccount(token=settings.plex_token.get_secret_value())
     novell = trace(acct.resource)('Novell')
     novell.connections = [c for c in novell.connections if not c.local]
@@ -19,7 +21,7 @@ def get_plex(settings=Depends(get_settings)) -> PlexServer:
 
 
 @trace
-def get_imdb_in_plex(imdb_id: ImdbId, plex) -> Media | None:
+def get_imdb_in_plex(imdb_id: ImdbId, plex: PlexServer) -> Media | None:
     guid = f"com.plexapp.agents.imdb://{imdb_id}?lang=en"
     items = trace(plex.library.search)(guid=guid)
     return items[0] if items else None
