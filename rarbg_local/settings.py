2d511887ed3182463c21f0fa6f715a507fc87c69
Elliana May <me@mause.me>
2025-06-21 17:13:53 +0800

    rework app fixture

--- rarbg_local/settings.py
+++ rarbg_local/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
+from typing import Annotated
 
-from pydantic import SecretStr
+from pydantic import AnyUrl, SecretStr
 from pydantic_settings import BaseSettings
 
 from .singleton import singleton
@@ -10,6 +11,10 @@ class Settings(BaseSettings):
     root: Path = Path(__file__).parent.parent.absolute()
     database_url: str = f"sqlite:///{root}/db.db"
     static_resources_path: Path = root / 'app/build'
+    transmission_url: Annotated[str, AnyUrl] = 'http://localhost:9091'
+    '''
+    TODO: use this
+    '''
     plex_token: SecretStr
 
 
