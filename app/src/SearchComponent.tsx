1eb4a8dbada2aea18d540837a75071a07e8e689e
Elliana May <me@mause.me>
2025-06-22 13:03:48 +0800

    use isValidating for loading bar

--- app/src/SearchComponent.tsx
+++ app/src/SearchComponent.tsx
@@ -12,32 +12,31 @@ export function SearchComponent() {
   const { search } = useLocation();
   const query = new URLSearchParams(search.slice(1)).get('query')!;
 
-  const { data: results, error } = useSWR<SearchResult[], Error>(
-    'search?' + qs.stringify({ query }),
-  );
+  const {
+    data: results,
+    error,
+    isValidating,
+  } = useSWR<SearchResult[], Error>('search?' + qs.stringify({ query }));
   return (
     <div>
       <SearchBox />
       {error && <DisplayError error={error} />}
+      {isValidating && <ReactLoading type="balls" color="#000" />}
       <ul>
-        {results ? (
-          results.map((result) => (
-            <li key={result.tmdb_id}>
-              <MLink
-                to={
-                  result.type === 'movie'
-                    ? `/select/${result.tmdb_id}/options`
-                    : `/select/${result.tmdb_id}/season`
-                }
-                state={{ query }}
-              >
-                {result.title} ({result.year ? result.year : 'Unknown year'})
-              </MLink>
-            </li>
-          ))
-        ) : (
-          <ReactLoading type="balls" color="#000" />
-        )}
+        {results?.map((result) => (
+          <li key={result.tmdb_id}>
+            <MLink
+              to={
+                result.type === 'movie'
+                  ? `/select/${result.tmdb_id}/options`
+                  : `/select/${result.tmdb_id}/season`
+              }
+              state={{ query }}
+            >
+              {result.title} ({result.year ? result.year : 'Unknown year'})
+            </MLink>
+          </li>
+        ))}
       </ul>
       {results && results.length === 0 ? 'No results' : null}
     </div>
