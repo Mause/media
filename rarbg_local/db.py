36c558fb9fd451ecd75a3a9ece2658da060c5053
Elliana May <me@mause.me>
2025-06-26 12:04:55 +0800

    expand fix

--- rarbg_local/db.py
+++ rarbg_local/db.py
@@ -340,7 +340,7 @@ async def get_or_create[T](
 
 def normalise_db_url(database_url: str) -> URL:
     parsed = make_url(database_url)
-    if parsed.get_backend_name() == 'postgres':
+    if parsed.get_backend_name() in ('postgres', 'postgresql'):
         parsed = parsed.set(drivername='postgresql+psycopg')
     return parsed
 
