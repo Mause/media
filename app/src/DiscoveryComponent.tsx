5f9a87d2e69454b23ca491fd0279b2283e934825
Elliana May <me@mause.me>
2025-07-01 00:20:27 +0800

    just use useSWR

--- app/src/DiscoveryComponent.tsx
+++ app/src/DiscoveryComponent.tsx
@@ -1,28 +1,19 @@
-import type { LoaderFunction } from 'react-router-dom';
-import { useLoaderData } from 'react-router-dom';
+import useSWR from 'swr';
 import { Grid } from '@mui/material';
 
 import type { paths } from './schema';
+import { Loading } from './render';
 
 type DiscoverResponse =
   paths['/api/discover']['get']['responses']['200']['content']['application/json'];
 
-export const loader = (async () => {
-  // TODO: auth
-  const res = await fetch('/api/discover');
-
-  const data = (await res.json()) as unknown as DiscoverResponse;
-
-  return { data };
-}) satisfies LoaderFunction;
-
 export function DiscoveryComponent() {
-  const { data } = useLoaderData<typeof loader>();
+  const { data, isValidating } = useSWR<DiscoverResponse>('/api/discover');
 
   return (
     <div>
-      <h3>Discovery Component</h3>
-      <p>This is a placeholder for the Discovery component.</p>
+      <h3>DiscoveryComponent</h3>
+      <Loading loading={isValidating} />
       <Grid container spacing={2}>
         {data?.results.map((result) => (
           <Grid>
