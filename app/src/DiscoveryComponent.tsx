07ebbee2a5f5b395eaba57d23363aa8da719151d
Elliana May <me@mause.me>
2025-07-01 11:18:33 +0800

    add link to search

--- app/src/DiscoveryComponent.tsx
+++ app/src/DiscoveryComponent.tsx
@@ -3,7 +3,12 @@ import { Grid } from '@mui/material';
 
 import type { paths } from './schema';
 import { Loading } from './render';
+import { MLink } from './MLink';
 import { DisplayError } from './IndexComponent';
+import {
+  faSearch,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 type DiscoverResponse =
   paths['/api/discover']['get']['responses']['200']['content']['application/json'];
@@ -15,13 +20,18 @@ export function DiscoveryComponent() {
 
   return (
     <div>
-      <h3>DiscoveryComponent</h3>
+      <h3>Discovery</h3>
       <Loading loading={isValidating} />
       {error && <DisplayError error={error} />}
       <Grid container spacing={2}>
         {data?.results.map((result) => (
           <Grid>
-            <h4>{result.title}</h4>
+            <h4>
+              {result.title}
+              <MLink to={`/select/${tmdb_id}/options`}>
+                <FontAwesomeIcon icon={faSearch} />
+              </MLink>
+            </h4>
             <p>{result.overview}</p>
           </Grid>
         ))}
