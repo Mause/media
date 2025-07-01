61787f1214ddb421067517f75cdfd267019bfffc
Elliana May <me@mause.me>
2025-07-01 12:11:06 +0800

    add react router vite plugin

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,4 +1,4 @@
-import react from '@vitejs/plugin-react';
+import react from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import bundlesize from 'vite-plugin-bundlesize';
