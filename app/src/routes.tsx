673c5d0c8de905b8fc6272bfe04c50a9a9a10fbb
Elliana May <me@mause.me>
2025-07-02 04:10:34 +0000

    title Websocket

--- app/src/routes.tsx
+++ app/src/routes.tsx
@@ -28,11 +28,7 @@ export function getRoutes() {
           lazy: async () => {
             const { Websocket } = await import('./Websocket');
             return {
-              element: (
-                <RouteTitle title="Websocket">
-                  <Websocket />
-                </RouteTitle>
-              ),
+              Component: Websocket,
             };
           },
         },
