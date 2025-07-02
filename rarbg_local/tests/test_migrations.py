d79cf9938404bc9927e2e296c926eec5e1f5a7cd
Elliana May <me@mause.me>
2025-07-03 01:08:46 +0800

    enable more tests

--- rarbg_local/tests/test_migrations.py
+++ rarbg_local/tests/test_migrations.py
@@ -3,10 +3,14 @@ from pathlib import Path
 from pytest import fixture
 from pytest_alembic.config import Config
 from pytest_alembic.tests import (
-    # test_model_definitions_match_ddl,
+    test_model_definitions_match_ddl,
     test_single_head_revision,
-    # test_up_down_consistency,
-    # test_upgrade,
+    test_up_down_consistency,
+    test_upgrade,
+)
+from pytest_alembic.tests.experimental import (
+    # test_all_models_register_on_metadata,
+    test_downgrade_leaves_no_trace,
 )
 from sqlalchemy import URL
 
@@ -23,8 +27,10 @@ def alembic_config(tmp_path: Path) -> Config:
 
 
 __all__ = [
-    # "test_upgrade",
-    # "test_model_definitions_match_ddl",
+    "test_upgrade",
+    "test_model_definitions_match_ddl",
+    # "test_all_models_register_on_metadata",
     "test_single_head_revision",
-    # "test_up_down_consistency",
+    "test_downgrade_leaves_no_trace",
+    "test_up_down_consistency",
 ]
