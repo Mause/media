eb177dfa5e9c6f6e645734de8a198ddd201a61c1
Elliana May <me@mause.me>
2025-07-03 00:41:22 +0800

    add pytest_alembic config fixture

--- rarbg_local/tests/test_migrations.py
+++ rarbg_local/tests/test_migrations.py
@@ -1,9 +1,23 @@
+from pathlib import Path
+
+from pytest import fixture
+from pytest_alembic.config import Config
 from pytest_alembic.tests import (
     # test_model_definitions_match_ddl,
     test_single_head_revision,
     # test_up_down_consistency,
     # test_upgrade,
 )
+
+
+@fixture
+def alembic_config(tmp_path: Path) -> Config:
+    return Config(
+        config_options={
+            'sqlalchemy.url': f'sqlite://{tmp_path}/db.db',
+        }
+    )
+
 
 __all__ = [
     # "test_upgrade",
