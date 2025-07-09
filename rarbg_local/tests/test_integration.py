22a2dc115fe082abb8cafe918bd7ed53fdfd7d63
Elliana May <me@mause.me>
2025-07-09 11:33:10 +0800

    use int ratingKey

--- rarbg_local/tests/test_integration.py
+++ rarbg_local/tests/test_integration.py
@@ -761,7 +761,7 @@ async def test_plex_redirect(
         responses,
         'GET',
         'https://test/library/all?guid=com.plexapp.agents.imdb%3A%2F%2Ftt10000%3Flang%3Den',
-        E.Search(E.Video(type='Video.episode', ratingKey='aaa')),
+        E.Search(E.Video(type='Video.episode', ratingKey=str(666))),
     )
 
     add_xml(
@@ -781,7 +781,7 @@ async def test_plex_redirect(
 
     assert (
         r.headers['Location']
-        == 'https://app.plex.tv/desktop#!/server/aaaa/details?key=%2Flibrary%2Fmetadata%2Faaa'
+        == 'https://app.plex.tv/desktop#!/server/aaaa/details?key=%2Flibrary%2Fmetadata%2F666'
     )
 
     r = await test_client.get('/api/plex/imdb/tt10000')
