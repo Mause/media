4ba3d5d97e2b3752f8564fd261c3e2cda918bcdc
Elliana May <me@mause.me>
2025-07-16 21:14:11 +0800

    merge index.tsx into root.tsx

--- app/src/root.tsx
+++ app/src/root.tsx
@@ -1,5 +1,24 @@
 import type { ReactNode } from 'react';
 import { Scripts, Outlet } from 'react-router-dom';
+import { StrictMode } from 'react';
+import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
+import {
+  ThemeProvider,
+  StyledEngineProvider,
+  createTheme,
+} from '@mui/material/styles';
+
+const theme = createTheme();
+
+const clientId = import.meta.env.REACT_APP_AUTH0_CLIENT_ID;
+const audience = import.meta.env.REACT_APP_AUTH0_AUDIENCE;
+
+if (!(clientId && audience)) {
+  console.error(
+    'Missing Auth0 client ID or audience. Please check your environment variables.',
+  );
+}
 
 export function Layout({ children }: { children: ReactNode }) {
   return (
@@ -17,7 +36,23 @@ export function Layout({ children }: { children: ReactNode }) {
         <title>Media</title>
       </head>
       <body>
-        {children}
+        <StrictMode>
+          <Auth0Provider
+            domain="mause.au.auth0.com"
+            clientId={clientId!}
+            authorizationParams={{
+              audience: audience!,
+              //   redirect_uri: window.location.origin,
+            }}
+            useRefreshTokensFallback={true}
+            useRefreshTokens={true}
+            cacheLocation="localstorage"
+          >
+            <StyledEngineProvider injectFirst>
+              <ThemeProvider theme={theme}>{children}</ThemeProvider>
+            </StyledEngineProvider>
+          </Auth0Provider>
+        </StrictMode>
         <Scripts />
       </body>
     </html>
