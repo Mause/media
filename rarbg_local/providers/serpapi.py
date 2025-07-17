7ab39e1bb756a099b9cc28c614e4b58f8408de07
Elliana May <me@mause.me>
2025-07-17 12:45:49 +0800

    Create serpapi.py

---
+++ rarbg_local/providers/serpapi.py
@@ -0,0 +1,17 @@
+import aiohttp
+
+
+async def search(movie_name: str, location: str, api_key: str) -> dict:
+    async with aiohttp.ClientSession() as session:
+        res = await session.get(
+            'https://serpapi.com/search.json',
+            params={
+                'q': f'{movie_name} show times',
+                'location': 'Austin, Texas, United States',
+                'hl': 'en',
+                'gl': 'us',
+                'apiKey': api_key
+            }
+        )
+        res.raise_for_status()
+        return await res.json()
