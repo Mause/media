364a66c08229de79afcb67b9e6670efe8600f0cb
Elliana May <me@mause.me>
2025-07-19 18:34:52 +0800

    add ssr config

--- app/vite.config.js
+++ app/vite.config.js
@@ -22,6 +22,15 @@ export default defineConfig({
     outDir: 'build',
     sourcemap: 'hidden',
   },
+  ssr: {
+    // Workaround for resolving dependencies in the server bundle
+    // Without this, the React context will be different between direct import and transitive imports in development environment
+    // For more information, see https://github.com/mui/material-ui/issues/45878#issuecomment-2987441663
+    optimizeDeps: {
+      include: ['@emotion/*', '@mui/*'],
+    },
+    noExternal: ['@emotion/*', '@mui/*'],
+  },
   server: {
     open: true,
     port: 3000,
