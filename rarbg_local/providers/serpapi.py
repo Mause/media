2a7f50578ac830e8fd25c079aec15694d50b7290
Elliana May <me@mause.me>
2025-07-20 15:53:10 +0800

    add demo

--- rarbg_local/providers/serpapi.py
+++ rarbg_local/providers/serpapi.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime, timezone
 from typing import Annotated, Any
 
@@ -8,6 +9,7 @@ from geoip2.models import City
 from geoip_api.core.lookup import get_database_path
 from pydantic import BaseModel, GetCoreSchemaHandler, ValidatorFunctionWrapHandler
 from pydantic_core import CoreSchema, core_schema
+from rich import print
 
 
 class HumanDate:
@@ -41,6 +43,8 @@ class Showtime(BaseModel):
 class KnowledgeGraph(BaseModel):
     title: str
     description: str
+    # image: str
+    # url: str
     rating: float | None = None
     rating_count: int | None = None
     release_date: Annotated[datetime | None, HumanDate()] = None
@@ -82,3 +86,41 @@ def get_age() -> datetime:
     with geoip2.database.Reader(get_database_path()) as city_reader:
         m = city_reader.metadata()
         return datetime.fromtimestamp(m.build_epoch, timezone.utc)
+
+
+async def main() -> None:
+    import os
+
+    import dotenv
+
+    with open('data.json') as fh:
+        data = SearchResult.model_validate_json(fh.read())
+
+    print(data)
+
+    dotenv.load_dotenv()
+
+    ip_address = '203.12.14.33'
+    location = resolve_location(ip_address)
+
+    movie_name = 'The Phoenician Scheme'
+    js = await search(
+        movie_name=movie_name,
+        location=', '.join(
+            [
+                location.city.name,
+                location.subdivisions.most_specific.name,
+                location.country.name,
+            ]
+        ),
+        iso_code=location.country.iso_code or 'au',
+        api_key=os.environ['SERP_API_KEY'],
+    )
+    with open(f'{movie_name}.json', 'w') as fh:
+        json.dump(js, fh, indent=2)
+
+
+if __name__ == '__main__':
+    import uvloop
+
+    uvloop.run(main())
