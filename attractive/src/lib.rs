274289f925befe737cb3d14b4dbf0fbdcb9ce60e
Elliana May <me@mause.me>
2025-08-12 22:00:16 +0800

    cargo fmt

--- attractive/src/lib.rs
+++ attractive/src/lib.rs
@@ -23,18 +23,18 @@ impl ITorrent {
 pub(crate) fn search_leetx(py: Python, term: String) -> Result<Bound<'_, PyAny>, PyErr> {
     pyo3_async_runtimes::tokio::future_into_py(py, async move {
         torrent_search::search_l337x(term)
-        .await
-        .map(|list| {
+            .await
+            .map(|list| {
                 list.into_iter()
-                .map(|movie| ITorrent {
-                    title: movie.name,
-                    leechers: movie.leeches.unwrap_or(0) as i32,
-                    seeders: movie.seeders.unwrap_or(0),
-                    magnet: movie.magnet.unwrap_or("".to_string()),
-                    category: "Unknown".to_string(),
-                })
-                .collect::<Vec<ITorrent>>()
-        })
+                    .map(|movie| ITorrent {
+                        title: movie.name,
+                        leechers: movie.leeches.unwrap_or(0) as i32,
+                        seeders: movie.seeders.unwrap_or(0),
+                        magnet: movie.magnet.unwrap_or("".to_string()),
+                        category: "Unknown".to_string(),
+                    })
+                    .collect::<Vec<ITorrent>>()
+            })
             .map_err(|e| pyo3::exceptions::PyValueError::new_err(format!("Error: {e}")))
     })
 }
@@ -45,21 +45,28 @@ pub fn search_yts(py: Python, term: String) -> Result<Bound<'_, PyAny>, PyErr> {
         yts_api::ListMovies::new()
             .query_term(&term.replace(" ", "+"))
             .limit(50)
-        .execute()
-        .await
-        .map(|list| {
-            let vec = list.movies.iter().filter(|movie| movie.imdb_code == term).collect::<Vec<&Movie>>();
-            vec.first()
-                .map(|movie| movie.torrents.iter()
-                .map(|torrent| ITorrent {
-                    magnet: format!("magnet:{}", torrent.hash),
-                    seeders: torrent.seeds,
-                    leechers: -1, // movie.peers - movie.seeds,
-                    title: format!("{} {}", movie.title, torrent.quality),
-                    category: format!("{} {}", torrent._type, torrent.quality),
+            .execute()
+            .await
+            .map(|list| {
+                let vec = list
+                    .movies
+                    .iter()
+                    .filter(|movie| movie.imdb_code == term)
+                    .collect::<Vec<&Movie>>();
+                vec.first().map(|movie| {
+                    movie
+                        .torrents
+                        .iter()
+                        .map(|torrent| ITorrent {
+                            magnet: format!("magnet:{}", torrent.hash),
+                            seeders: torrent.seeds,
+                            leechers: -1, // movie.peers - movie.seeds,
+                            title: format!("{} {}", movie.title, torrent.quality),
+                            category: format!("{} {}", torrent._type, torrent.quality),
+                        })
+                        .collect::<Vec<ITorrent>>()
                 })
-                .collect::<Vec<ITorrent>>())
-        })
+            })
             .map_err(|e| pyo3::exceptions::PyValueError::new_err(format!("Error: {e}")))
     })
 }
