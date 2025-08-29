0ea64c81d8bab41884460cad82b9b4be6a791a98
Elliana May <me@mause.me>
2025-08-29 18:55:38 +0800

    sketch out provider

--- rarbg_local/providers/luna.py
+++ rarbg_local/providers/luna.py
@@ -1,14 +1,46 @@
-import requests
+import json
+from typing import AsyncGenerator
+
+import aiohttp
+from healthcheck import HealthcheckCallbackResponse, HealthcheckStatus
+
+from .abc import ITorrent, MovieProvider, ProviderSource
 
 url = "http://luna-leederville.3cx.com.au:4025/VenueSchedule.json"
 
 
-def get_venue_schedule():
-    response = requests.get(url)
-    response.encoding = 'utf-16-le'
-    response.raise_for_status()
-    return response.json()
+async def get_venue_schedule() -> dict:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.json(content_type=None, encoding='utf-16-le')
 
 
-breakpoint()
-weird = get_venue_schedule()
+class LunaProvider(MovieProvider):
+    async def search_for_movie(self) -> AsyncGenerator[ITorrent, None]:
+        schedule = await get_venue_schedule()
+        for item in schedule['Items']:
+            title = item['Title']
+            yield ITorrent(
+                title=title,
+                source=ProviderSource.LUNA,
+                seeders=1,
+                download=f"http://luna-leederville.3cx.com.au:4025/Download/{item['Id']}",
+                category="Movie",
+            )
+
+    async def health(self) -> HealthcheckCallbackResponse:
+        await get_venue_schedule()
+        return HealthcheckCallbackResponse(HealthcheckStatus.PASS, "OK")
+
+
+async def main() -> None:
+    schedule = await get_venue_schedule()
+
+    with open('luna_venue_schedule.json', 'w') as f:
+        json.dump(schedule, f, indent=2)
+
+
+if __name__ == "__main__":
+    import uvloop
+
+    uvloop.run(main())
