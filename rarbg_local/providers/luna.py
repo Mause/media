8e402d324a2693989a829eb0c2cfdfd8aefd377e
Elliana May <me@mause.me>
2025-08-29 20:00:22 +0800

    add models

--- rarbg_local/providers/luna.py
+++ rarbg_local/providers/luna.py
@@ -1,12 +1,52 @@
 import json
-from typing import AsyncGenerator
+from typing import Annotated, AsyncGenerator, Callable
 
 import aiohttp
 from healthcheck import HealthcheckCallbackResponse, HealthcheckStatus
+from pydantic import BaseModel, ConfigDict, Field
 
-from .abc import ITorrent, MovieProvider, ProviderSource
+from .abc import ImdbId, ITorrent, MovieProvider, ProviderSource, TmdbId
 
 url = "http://luna-leederville.3cx.com.au:4025/VenueSchedule.json"
+
+
+def mk(prefix: str) -> Callable[[str], str]:
+    def to_title(s: str) -> str:
+        return prefix + s.title()
+
+    return to_title
+
+
+class Base(BaseModel):
+    model_config = ConfigDict(
+        alias_generator=mk(''),
+        populate_by_name=True,
+        populate_by_alias=True,
+    )
+
+
+class Movie(Base):
+    model_config = ConfigDict(
+        alias_generator=mk("Movie_"),
+    )
+    name: str
+    imdb_id: Annotated[str | None, Field(alias='Movie_IMDB_ID')]
+    tmdb_id: Annotated[str | None, Field(alias='Movie_TMDb_ID')]
+
+    url: Annotated[str, Field(alias='Movie_URL')]
+    code: str
+
+
+class Session(Base):
+    model_config = ConfigDict(
+        alias_generator=mk("Session_"),
+    )
+    index: int
+
+
+class Schedule(Base):
+    sessions: list[Session]
+    movies: list[Movie]
 
 
 async def get_venue_schedule() -> dict:
@@ -16,7 +56,9 @@ async def get_venue_schedule() -> dict:
 
 
 class LunaProvider(MovieProvider):
-    async def search_for_movie(self) -> AsyncGenerator[ITorrent, None]:
+    async def search_for_movie(
+        self, imdb_id: ImdbId, tmdb_id: TmdbId
+    ) -> AsyncGenerator[ITorrent, None]:
         schedule = await get_venue_schedule()
         for item in schedule['Items']:
             title = item['Title']
@@ -34,6 +76,9 @@ class LunaProvider(MovieProvider):
 
 
 async def main() -> None:
+    with open('luna_venue_schedule.json', 'r') as f:
+        Schedule.model_validate_json(f.read())
+
     schedule = await get_venue_schedule()
 
     with open('luna_venue_schedule.json', 'w') as f:
