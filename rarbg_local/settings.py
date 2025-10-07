5148cbf756dddeff9ce365147e27656c70550307
Elliana May <me@mause.me>
2025-10-07 14:00:55 +0800

    increase secrecy

--- rarbg_local/settings.py
+++ rarbg_local/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from typing import Annotated
 
-from pydantic import AnyUrl, Field, SecretStr
+from pydantic import AnyUrl, Field, Secret, SecretStr
 from pydantic_settings import BaseSettings
 
 from .singleton import singleton
@@ -9,14 +9,14 @@ from .singleton import singleton
 
 class Settings(BaseSettings):
     root: Path = Path(__file__).parent.parent.absolute()
-    database_url: str = f"sqlite:///{root}/db.db"
+    database_url: Annotated[str, SecretStr] = f"sqlite:///{root}/db.db"
     static_resources_path: Path = root / 'app/build'
     transmission_url: Annotated[str, AnyUrl] = 'http://localhost:9091'
     '''
     TODO: use this
     '''
     plex_token: SecretStr
-    cache_url: Annotated[str, Field(AnyUrl, alias='REDISCLOUD_URL')] = (
+    cache_url: Annotated[str, Field(Secret[AnyUrl], alias='REDISCLOUD_URL')] = (
         'memory://'
     )
 
