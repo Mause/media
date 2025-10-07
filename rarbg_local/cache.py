0b300b2ee9b0347b41b73c605e7a91886ed0fb54
Elliana May <me@mause.me>
2025-10-07 14:36:14 +0800

    add todo

--- rarbg_local/cache.py
+++ rarbg_local/cache.py
@@ -8,4 +8,5 @@ from .settings import Settings, get_settings
 
 
 async def get_cache(settings: Annotated[Settings, Depends(get_settings)]) -> Cache:
+    # TODO: use context manager here to autoclose once supported
     return Cache.from_url(settings.cache_url)
