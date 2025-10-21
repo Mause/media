513160521e3ee3904618ec4e5567bb2f000f9464
Elliana May <me@mause.me>
2025-10-21 13:19:56 +0800

    inject key

--- rarbg_local/statsig_service.py
+++ rarbg_local/statsig_service.py
@@ -1,11 +1,13 @@
 from statsig_python_core import Statsig, StatsigOptions
 
+from .settings import get_settings
 
-async def statig_service(settings: Annotated[Settings, Depends(get_settings))) -> Statsig:
+
+async def statig_service(settings: Annotated[Settings, Depends(get_settings)]) -> Statsig:
     options = StatsigOptions()
     options.environment = "development"
 
-    statsig = Statsig("secret-key", options)
+    statsig = Statsig(settings.statsig_key, options)
     statsig.initialize().wait()
 
     return statsig
