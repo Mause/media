1cc3a485bf7e13543e5fd4cd6e3325730c3b26eb
Elliana May <me@mause.me>
2025-10-21 17:50:09 +0800

    Refactor Statsig integration and update imports

--- rarbg_local/sentry/statsig.py
+++ rarbg_local/sentry/statsig.py
@@ -4,11 +4,8 @@ from typing import TYPE_CHECKING
 from sentry_sdk.feature_flags import add_feature_flag
 from sentry_sdk.integrations import Integration, _check_minimum_version
 from sentry_sdk.utils import parse_version
-from statsig_python_core import statsig as statsig_module
+from statsig_python_core as Statsig, StatsigUser
 from statsig_python_core.version import __version__ as STATSIG_VERSION
-
-if TYPE_CHECKING:
-    pass
 
 
 class StatsigIntegration(Integration):
@@ -21,14 +18,14 @@ class StatsigIntegration(Integration):
         _check_minimum_version(StatsigIntegration, version, "statsig")
 
         # Wrap and patch evaluation method(s) in the statsig module
-        old_check_gate = statsig_module.Statsig.check_gate
+        old_check_gate = Statsig.check_gate
 
         @wraps(old_check_gate)
         def sentry_check_gate(
-            user: statsig_module.StatsigUser, gate: str, *args, **kwargs
-        ):
-            enabled = old_check_gate(user, gate, *args, **kwargs)
+            user: StatsigUser, gate: str, options: FeatureGateEvaluationOptions | None = None
+        ) -> bool:
+            enabled = old_check_gate(user, gate, options)
             add_feature_flag(gate, enabled)
             return enabled
 
-        statsig_module.Statsig.check_gate = sentry_check_gate
+        Statsig.check_gate = sentry_check_gate
