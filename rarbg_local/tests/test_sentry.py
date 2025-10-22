299770ddb78d57f34df65fb11d6db0d22b076b9a
Elliana May <me@mause.me>
2025-10-22 10:47:26 +0800

    don't use instance

--- rarbg_local/tests/test_sentry.py
+++ rarbg_local/tests/test_sentry.py
@@ -2,6 +2,5 @@ from ..sentry.statsig import StatsigIntegration
 
 
 def test_statsig_integration() -> None:
-    integration = StatsigIntegration()
-    integration.setup_once()
-    assert integration.identifier == "statsig-python-core"
+    StatsigIntegration.setup_once()
+    assert StatsigIntegration.identifier == "statsig-python-core"
