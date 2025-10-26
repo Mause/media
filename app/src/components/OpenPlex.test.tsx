e426ce9c992fc14d4565026bebc1f984200c7750
Elliana May <me@mause.me>
2025-10-27 00:42:26 +0800

    get rendering working

--- app/src/components/OpenPlex.test.tsx
+++ app/src/components/OpenPlex.test.tsx
@@ -1,17 +1,46 @@
-import { OpenPlex } from './OpenPlex';
+import { OpenPlex, ContextMenu } from '.';
 import { describe, test } from 'vitest';
 import { renderWithSWR } from '../test.utils';
+import userEvent from '@testing-library/user-event';
+import { act, screen } from '@testing-library/react';
+import { server } from '../msw';
+import { http, HttpResponse } from 'msw';
 
 describe('OpenPlex', () => {
-  test('should render without crashing', () => {
-    const el = renderWithSWR(
-      <OpenPlex
-        download={{
-          tmdb_id: 12345,
-        }}
-        type="movie"
-      />,
+  test('should render without crashing', async () => {
+    const { container } = renderWithSWR(
+      <ContextMenu>
+        <OpenPlex
+          download={{
+            tmdb_id: 12345,
+          }}
+          type="movie"
+        />
+      </ContextMenu>,
     );
-    expect(el.container).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
+
+    const events = userEvent.setup();
+
+    // server.use(
+    //   http.post('/api/monitor', async ({ request }) => {
+    //     expect(await request.json()).toEqual({
+    //       type: 'MOVIE',
+    //       tmdb_id: 5,
+    //     });
+    //     return HttpResponse.json({});
+    //   }),
+    // );
+    // server.use(
+    //   http.get('/api/monitor', () => {
+    //     return HttpResponse.json([] satisfies MonitorResponse);
+    //   }),
+    //   3,
+    // );
+
+    await events.click(await screen.findByTestId('context-menu-open'));
+    await events.click(await screen.findByText('Open in Plex'));
+
+    expect(container).toMatchSnapshot();
   });
 });
