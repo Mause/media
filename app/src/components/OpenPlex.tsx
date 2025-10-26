c9ec22bcc1cc2ced94240888515a41858577e45b
Elliana May <me@mause.me>
2025-10-26 22:48:37 +0800

    extract OpenPlex

---
+++ app/src/components/OpenPlex.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import _ from 'lodash';
+import usePromise from 'react-promise-suspense';
+import { nextId, readyStateToString, useMessage } from './websocket';
+import { OpenNewWindow } from './OpenNewWindow';
+import type { components } from '../schema';
+import { getToken } from '../utils';
+import MenuItem from '@mui/material/MenuItem';
+// eslint-disable-next-line import-x/no-named-as-default
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+
+type PlexRootResponse = components['schemas']['PlexRootResponse'];
+type PlexRequest = components['schemas']['PlexRequest'];
+
+export function OpenPlex({
+  download,
+  type,
+}: {
+  download: { tmdb_id: number };
+  type: 'movie' | 'tv';
+}) {
+  const auth = useAuth0();
+  const [open, setOpen] = useState(false);
+  const token = 'Bearer ' + usePromise(() => getToken(auth), []);
+  const { message, trigger, readyState, state } = useMessage<
+    PlexRequest,
+    PlexRootResponse
+  >({
+    method: 'plex',
+    jsonrpc: '2.0',
+    id: nextId(),
+    authorization: token,
+    params: {
+      tmdb_id: download.tmdb_id,
+      media_type: type,
+    },
+  });
+
+  if (message) {
+    const first = _.values(message.result).find((v) => v)!;
+    return <OpenNewWindow link={first.link} label={first.item.title} />;
+  }
+
+  return (
+    <>
+      <Dialog open={open}>
+        <DialogTitle>Search plex for media...</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {readyStateToString(readyState)}
+            <br />
+            {state}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)}>Cancel</Button>
+        </DialogActions>
+      </Dialog>
+      <MenuItem
+        onClick={() => {
+          setOpen(true);
+          trigger();
+        }}
+      >
+        <span className="unselectable">Open in Plex</span>
+      </MenuItem>
+    </>
+  );
+}
