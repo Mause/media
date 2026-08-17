a6c59f03f3e3a2af42b67171d6d6d4729707b7c0
Elliana May <me@mause.me>
2026-08-17 21:48:43 +1000

    tick

--- undo.py
+++ undo.py
@@ -1,18 +1,25 @@
+import logging
+from collections.abc import Generator
 from csv import DictReader
 from typing import cast
 
-import pudb
 import sqlglot
+from rich.logging import RichHandler
 from sqlglot.dialects import Postgres
 from sqlglot.expressions import Copy
 
+logging.basicConfig(level=logging.INFO, handlers=[RichHandler()])
 
-def parse_sql_backup(file_path):  # noqa: ANN201
+dialect = Postgres()
+logger = logging.getLogger(__name__)
+
+
+def parse_sql_backup(file_path: str) -> Generator[tuple[Copy, list[str]]]:
     with open(file_path, encoding='utf-8') as f:
         for line in f:
             if not line.startswith('COPY '):
                 continue  # Skip lines that do not start with 'COPY '
-            clause = cast(Copy, sqlglot.parse_one(line, dialect=Postgres()))
+            clause = cast(Copy, sqlglot.parse_one(line, dialect))
             group = []
             while True:
                 next_line = f.readline()
@@ -20,17 +27,14 @@ def parse_sql_backup(file_path):  # noqa: ANN201
                     break
                 group.append(next_line)
 
-            yield (
-                clause,
-                group,
-            )  # Yield the COPY clause and the corresponding data group
+            yield (clause, group)
 
 
-# Usage
-
-pudb.set_trace()  # Set a breakpoint for debugging
 for clause, csv in parse_sql_backup('latest.dump.sql'):
-    for line in DictReader(csv, ['a'], delimiter='\t', quoting=3):
-        print(line)
-    # print(clause)
-    # print(csv)
+    table = clause.this.this.name
+    columns = [expr.name for expr in clause.this.expressions]
+    logger.info(
+        '%s: %s',
+        table,
+        [line for line in DictReader(csv, columns, delimiter='\t', quoting=3)],
+    )
